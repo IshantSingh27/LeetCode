6181dccc856f40387ea53629aa2ecdb9418b7128
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-01-27 09:14:14 +0530

    Time: 3 ms (74.73%), Space: 14.8 MB (35.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,27 +1,45 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         vector<pair<int , int>> arr(n);
+//         for(int i=0 ; i<n ; i++){
+//             arr[i].first = nums[i];
+//             arr[i].second = i;
+//         }
+
+//         sort(arr.begin() , arr.end());
+
+//         int i=0 , j=n-1;
+//         while(i < j){
+//             if(arr[i].first + arr[j].first == target){
+//                 return {arr[i].second , arr[j].second};
+//             }
+//             else if(arr[i].first + arr[j].first < target){
+//                 i++;
+//             }
+//             else{
+//                 j--;
+//             }
+//         }
+//         return {-1 , -1};
+//     }
+// };
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<pair<int , int>> arr(n);
+        unordered_map<int , int> mp;
+
         for(int i=0 ; i<n ; i++){
-            arr[i].first = nums[i];
-            arr[i].second = i;
+            int rem = target - nums[i];
+            if(mp.find(rem) != mp.end()){
+                return {mp[rem] , i};
+            }
+            mp[nums[i]] = i;
         }
 
-        sort(arr.begin() , arr.end());
-
-        int i=0 , j=n-1;
-        while(i < j){
-            if(arr[i].first + arr[j].first == target){
-                return {arr[i].second , arr[j].second};
-            }
-            else if(arr[i].first + arr[j].first < target){
-                i++;
-            }
-            else{
-                j--;
-            }
-        }
-        return {-1 , -1};
+        return{-1 , -1};
     }
 };
