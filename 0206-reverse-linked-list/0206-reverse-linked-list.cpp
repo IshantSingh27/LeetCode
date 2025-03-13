599a47846807d837a010f7f5eda8bc918e5cf2e3
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-03-13 14:38:55 +0530

    Time: 0 ms (100%), Space: 13.2 MB (99.45%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,21 +8,47 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         if(head == NULL || head->next == NULL) return head;
+//         ListNode* b1 = NULL;
+//         ListNode* a = head;
+//         ListNode* b2 = head->next;
+//         while(b2 != NULL){
+//             a->next = b1;
+//             b1 = a;
+//             a = b2;
+//             b2 = b2->next;
+//         }
+//         a->next = b1;
+//         head = a;
+
+//         return head;
+//     }
+// };
+
 class Solution {
 public:
+    void reverse(ListNode*& head , ListNode* b1 , ListNode* a , ListNode* b2){
+        if(b2 == NULL){
+            a->next = b1;
+            head = a;
+            return;
+        }
+
+        a->next = b1;
+        reverse(head , a , b2 , b2->next);
+    }
+
     ListNode* reverseList(ListNode* head) {
         if(head == NULL || head->next == NULL) return head;
+
         ListNode* b1 = NULL;
         ListNode* a = head;
         ListNode* b2 = head->next;
-        while(b2 != NULL){
-            a->next = b1;
-            b1 = a;
-            a = b2;
-            b2 = b2->next;
-        }
-        a->next = b1;
-        head = a;
+
+        reverse(head , b1 , a , b2);
 
         return head;
     }
