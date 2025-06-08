36601e3e22496ad399ce516e9225f6bb2c56d701
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-08 10:40:25 +0530

    Time: 1 ms (41.87%), Space: 16.5 MB (13.59%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int solve(int i , int j , vector<vector<int>>& grid , vector<vector<int>>& dp){
         if(i == 0 && j == 0) return grid[0][0];
-        if(i < 0 || j < 0) return 1e6;
+        if(i < 0 || j < 0) return 1e9;
         if(dp[i][j] != -1) return dp[i][j];
         int up = grid[i][j] + solve(i - 1 , j , grid , dp);
         int left = grid[i][j] + solve(i , j - 1 , grid , dp);
