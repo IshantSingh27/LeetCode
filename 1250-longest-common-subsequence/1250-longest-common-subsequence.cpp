ed3d5b20d60157c7d6a1ff35705e96e662197716
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-12 01:26:39 +0530

    Time: 27 ms (55.71%), Space: 27.4 MB (67.45%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -15,8 +15,8 @@ public:
         // return solve(n - 1 , m - 1 , s1 , s2 , dp);
         //TABULATION
         vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , 0));
-        // for(int i=0 ; i<=n ; i++) dp[i][0] = 0;
-        // for(int j=0 ; j<=m ; j++) dp[0][j] = 0;
+        for(int i=0 ; i<=n ; i++) dp[i][0] = 0; // base case
+        for(int j=0 ; j<=m ; j++) dp[0][j] = 0; // rather of no use
         for(int i=1 ; i<=n ; i++){
             for(int j=1 ; j<=m ; j++){
                 if(s1[i - 1] == s2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
