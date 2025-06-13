970277760e1e1ce9a2aee20bfb48acc1beb90416
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-14 02:31:48 +0530

    Time: 0 ms (100%), Space: 14 MB (94.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -89,19 +89,34 @@ public:
     //     cout<<endl;
     //     return 0;
     // }
+    // int lengthOfLIS(vector<int>& arr) {
+    //     int n = arr.size();
+    //     vector<int> ans;
+    //     ans.push_back(arr[0]);
+    //     for(int i=1 ; i<n ; i++){
+    //         if(arr[i] > ans.back()){
+    //             ans.push_back(arr[i]);
+    //         }
+    //         else{
+    //             int ind = lower_bound(ans.begin() , ans.end() , arr[i])  - ans.begin();
+    //             ans[ind] = arr[i];
+    //         }
+    //     }
+    //     return ans.size();
+    // }
     int lengthOfLIS(vector<int>& arr) {
-    int n = arr.size();
-    vector<int> ans;
-    ans.push_back(arr[0]);
-    for(int i = 1; i < n; i++) {
-        if(arr[i] > ans.back()) {
-            ans.push_back(arr[i]);
-        } else {
-            int ind = lower_bound(ans.begin(), ans.end(), arr[i]) - ans.begin();
-            ans[ind] = arr[i];
+        int n = arr.size();
+        vector<int> ans;
+        ans.push_back(arr[0]);
+        for(int i=1 ; i<n ; i++){
+            if(arr[i] > ans.back()){
+                ans.push_back(arr[i]);
+            }
+            else{
+                int ind = lower_bound(ans.begin() , ans.end() , arr[i]) - ans.begin();
+                ans[ind] = arr[i];
+            }
         }
+        return ans.size();
     }
-    return ans.size();
-}
-
 };
