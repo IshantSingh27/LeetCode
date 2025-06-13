941989fc7ac446036e65b887ea656e28994374bc
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-13 11:17:08 +0530

    Time: 21 ms (10.93%), Space: 40.3 MB (9.95%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -6,32 +6,30 @@ public:
             adj[it[0]].push_back({it[1] , it[2]});
             adj[it[1]].push_back({it[0] , it[2]});
         }
+        vector<long long> time(n , 1e18) , ways(n , 0);
+        time[0] = 0;
+        ways[0] = 1;
         priority_queue<pair<long long , long long> , vector<pair<long long , long long>> ,
         greater<pair<long long , long long>>> pq;
         pq.push({0 , 0});
-        vector<long long> dist(n , 1e18) , ways(n , 0);
-        dist[0] = 0;
-        ways[0] = 1;
-        long long mod = (long long)(1e9 + 7);
-
+        long long mod = 1e9 + 7;
         while(!pq.empty()){
             long long wt = pq.top().first;
             long long node = pq.top().second;
             pq.pop();
-
             for(auto it : adj[node]){
                 long long adjnode = it.first;
                 long long adjwt = it.second;
-                if(wt + adjwt < dist[adjnode]){
-                    dist[adjnode] = wt + adjwt;
-                    pq.push({dist[adjnode] , adjnode});
+                if((wt + adjwt) < time[adjnode]){
+                    time[adjnode] = (wt + adjwt);
                     ways[adjnode] = ways[node];
+                    pq.push({time[adjnode] , adjnode});
                 }
-                else if(wt + adjwt == dist[adjnode]){
+                else if((wt + adjwt) == time[adjnode]){
                     ways[adjnode] = (ways[adjnode] + ways[node]) % mod;
                 }
             }
         }
-        return ways[n-1] % mod;
+        return ways[n - 1];
     }
 };
