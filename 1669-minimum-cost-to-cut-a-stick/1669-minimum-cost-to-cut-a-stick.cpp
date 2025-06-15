cc289c403c142582d268a40bb222ef1c7a5f24d1
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-15 23:49:52 +0530

    Time: 39 ms (30.69%), Space: 14 MB (50.87%) - LeetHub

---
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(int i , int j , vector<int>& arr , vector<vector<int>>& dp){
+        if(i > j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int mini = 1e9;
+        for(int ind = i ; ind <= j ; ind++){
+            int cost = arr[j + 1] - arr[i - 1] + solve(i , ind - 1 , arr , dp) + solve(ind + 1 , j , arr , dp);
+            mini = min(mini , cost);
+        }
+        return dp[i][j] = mini;
+    }
+    int minCost(int n, vector<int>& arr) {
+        arr.push_back(n);
+        arr.insert(arr.begin() , 0);
+        sort(arr.begin() , arr.end());
+        int m = arr.size();
+        vector<vector<int>> dp(m , vector<int>(m , -1));
+        return solve(1 , m - 2 , arr , dp);
+    }
+};
