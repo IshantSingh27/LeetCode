25c682ab2406babbce7b3b468981fb059d88612b
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-16 00:17:54 +0530

    Time: 23 ms (64.14%), Space: 13.9 MB (50.87%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -15,7 +15,21 @@ public:
         arr.insert(arr.begin() , 0);
         sort(arr.begin() , arr.end());
         int m = arr.size();
-        vector<vector<int>> dp(m , vector<int>(m , -1));
-        return solve(1 , m - 2 , arr , dp);
+        // vector<vector<int>> dp(m , vector<int>(m , -1));
+        // return solve(1 , m - 2 , arr , dp);
+        //TABULATION
+        vector<vector<int>> dp(m , vector<int>(m , 0)); //base case 0 hi karna hai
+        for(int i = m - 2 ; i > 0 ; i--){
+            for(int j = i ; j <= m - 2 ; j++){ // obvio reverse se start hoga
+            if(i > j) continue;
+                int mini = 1e9;
+                for(int ind = i ; ind <= j ; ind++){
+                    int cost = arr[j + 1] - arr[i - 1] + dp[ind + 1][j] + dp[i][ind - 1];
+                    mini = min(mini , cost);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][m - 2];
     }
 };
