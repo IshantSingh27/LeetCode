35e10478c217e6ffc71e8f3a5e28dfaf70c2baa9
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-20 20:35:31 +0530

    Time: 3 ms (81.15%), Space: 14.1 MB (78.89%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -46,18 +46,61 @@ public:
     //     }
     //     return pre[0];
     // }
+    // int lengthOfLIS(vector<int>& arr) {
+    //     int n = arr.size();
+    //     int maxi = 1;
+    //     vector<int> dp(n , 1);
+    //     for(int ind = 0 ; ind < n ; ind++){
+    //         for(int prev = 0 ; prev <= ind ; prev++){
+    //             if(arr[ind] > arr[prev]){
+    //                 dp[ind] = max(dp[ind] , dp[prev] + 1);
+    //             }
+    //         }
+    //         maxi = max(maxi , dp[ind]);
+    //     }
+    //     return maxi;
+    // }
+    // int lengthOfLIS(vector<int>& arr) {
+    //     int n = arr.size();
+    //     int maxi = 1 , last = 0;
+    //     vector<int> dp(n , 1) , parent(n , 0);
+    //     for(int ind = 0 ; ind < n ; ind++){
+    //         parent[ind] = ind;
+    //         for(int prev = 0 ; prev < ind ; prev++){
+    //             if(arr[ind] > arr[prev] && dp[prev] + 1 > dp[ind]){
+    //                 dp[ind] = dp[prev] + 1;
+    //                 parent[ind] = prev;
+    //             }
+    //         }
+    //         if(dp[ind] > maxi){
+    //             maxi = dp[ind];
+    //             last = ind;
+    //         }
+    //     }
+    //     vector<int> ans;
+    //     while(parent[last] != last){
+    //         ans.push_back(arr[last]);
+    //         last = parent[last];
+    //     }
+    //     ans.push_back(last);
+    //     reverse(ans.begin() , ans.end());
+    //     for(auto it : ans){
+    //         cout<<it<<" ";
+    //     }
+    //     cout<<endl;
+    //     return 0;
+    // }
     int lengthOfLIS(vector<int>& arr) {
         int n = arr.size();
-        int maxi = 1;
-        vector<int> dp(n , 1);
-        for(int ind = 0 ; ind < n ; ind++){
-            for(int prev = 0 ; prev <= ind ; prev++){
-                if(arr[ind] > arr[prev]){
-                    dp[ind] = max(dp[ind] , dp[prev] + 1);
-                }
+        vector<int> ans;
+        ans.push_back(arr[0]);
+        for(int i=1 ; i<n ; i++){
+            if(arr[i] > ans.back()) ans.push_back(arr[i]);
+            else{
+                int ind = lower_bound(ans.begin() , ans.end() , arr[i]) - ans.begin();
+                ans[ind] = arr[i];
             }
-            maxi = max(maxi , dp[ind]);
         }
-        return maxi;
+        return ans.size();
     }
 };
