1383b8d571201610e7a9072dd3189faa6d5ef686
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-20 23:44:30 +0530

    Time: 10 ms (71.31%), Space: 12.5 MB (76.18%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> dp(n , 1) , parent(n , 0);
-        int maxi = 1 , last = 0;
         sort(arr.begin() , arr.end());
-        for(int cur = 1 ; cur < n ; cur++){
-            parent[cur] = cur;
-            for(int prev = 0 ; prev < cur ; prev++){
-                if(arr[cur] % arr[prev] == 0 && dp[cur] < dp[prev] + 1){
-                    dp[cur] = dp[prev] + 1;
-                    parent[cur] = prev;
+        int n = arr.size() , maxi = 0 , last = 0;
+        vector<int> dp(n , 1) , par(n , 0);
+        for(int ind = 0 ; ind < n ; ind++){
+            par[ind] = ind;
+            for(int prev = 0 ; prev < ind ; prev++){
+                if(arr[ind] % arr[prev] == 0){
+                    if(dp[prev] + 1 > dp[ind]){
+                        dp[ind] = dp[prev] + 1;
+                        par[ind] = prev;
+                    }
                 }
             }
-            if(dp[cur] > maxi){
-                maxi = dp[cur];
-                last = cur;
+            if(dp[ind] > maxi){
+                maxi = dp[ind];
+                last = ind;
             }
         }
         vector<int> ans;
-        while(parent[last] != last){
-            ans.push_back(arr[last]);
-            last = parent[last];
-        }
         ans.push_back(arr[last]);
+        while(par[last] != last){
+            ans.push_back(arr[par[last]]);
+            last = par[last];
+        }
         return ans;
     }
 };
