2d55b6049c9722311d6dee7bd760e18c5db85184
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-20 16:26:43 +0530

    Time: 263 ms (36.08%), Space: 294.7 MB (33.91%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -12,21 +12,38 @@ public:
         }
         return dp[ind][prev + 1] = max(take , notTake);
     }
+    // int lengthOfLIS(vector<int>& arr) {
+    //     int n = arr.size();
+    //     // vector<vector<int>> dp(n , vector<int>(n + 1 , -1));
+    //     // return solve(0 , -1 , arr , dp);
+    //     vector<vector<int>> dp(n + 1 , vector<int>(n + 1 , 0));
+    //     for(int ind = n-1 ; ind >= 0 ; ind--){
+    //         for(int prev = ind - 1 ; prev >= -1 ; prev--){
+    //             int notTake = 0 + dp[ind + 1][prev + 1];
+    //             int take = 0;
+    //             if(prev == -1 || arr[ind] > arr[prev]){
+    //                 take = 1 + dp[ind + 1][ind + 1];
+    //             }
+    //             dp[ind][prev + 1] = max(take , notTake);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
     int lengthOfLIS(vector<int>& arr) {
         int n = arr.size();
-        // vector<vector<int>> dp(n , vector<int>(n + 1 , -1));
-        // return solve(0 , -1 , arr , dp);
-        vector<vector<int>> dp(n + 1 , vector<int>(n + 1 , 0));
+        vector<int> pre(n + 1 , 0);
         for(int ind = n-1 ; ind >= 0 ; ind--){
+            vector<int> cur(n + 1 , 0);
             for(int prev = ind - 1 ; prev >= -1 ; prev--){
-                int notTake = 0 + dp[ind + 1][prev + 1];
+                int notTake = 0 + pre[prev + 1];
                 int take = 0;
                 if(prev == -1 || arr[ind] > arr[prev]){
-                    take = 1 + dp[ind + 1][ind + 1];
+                    take = 1 + pre[ind + 1];
                 }
-                dp[ind][prev + 1] = max(take , notTake);
+                cur[prev + 1] = max(take , notTake);
             }
+            pre = cur;
         }
-        return dp[0][0];
+        return pre[0];
     }
 };
