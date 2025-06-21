8662c988aa21cefb7442d3685be7cf3b352af500
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-21 17:53:49 +0530

    Time: 1848 ms (19.13%), Space: 651.3 MB (5.57%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    bool check(string& s1 , string& s2){
-        if(s1.size() + 1 != s2.size()) return false;
+    int check(string s1 , string s2){
+        if(s1.size() != 1 + s2.size()) return false;
         int first = 0 , second = 0;
-        while( (first < s1.size()) && (second < s2.size()) ){
+        while(first < s1.size() && second < s2.size()){
             if(s1[first] == s2[second]){
-                first++; second++;
+                first++;
+                second++;
             }
-            else second++;
+            else{
+                first++;
+            }
         }
-        if( first == s1.size() ) return true;
-        else return false;
+        if(second != s2.size()) return false;
+        else return true;
     }
-    static bool comp(string &s1 , string &s2){
+    static bool comp(string s1 , string s2){
         return s1.size() < s2.size();
     }
     int longestStrChain(vector<string>& arr) {
         sort(arr.begin() , arr.end() , comp);
-        int n = arr.size();
+        int n = arr.size() , maxi = 0;
         vector<int> dp(n , 1);
-        int maxi = 1;
-        for(int cur = 1 ; cur < n ; cur++){
-            for(int prev = 0 ; prev < cur ; prev++){
-                if(check(arr[prev] , arr[cur]) && dp[cur] < dp[prev] + 1){
-                    dp[cur] = dp[prev] + 1;
+        for(int ind = 0 ; ind < n ; ind++){
+            for(int prev = 0 ; prev < ind ; prev++){
+                if(check(arr[ind] , arr[prev]) && dp[ind] < dp[prev] + 1){
+                    dp[ind] = dp[prev] + 1;
                 }
             }
-            maxi = max(maxi , dp[cur]);
+            maxi = max(dp[ind] , maxi);
         }
         return maxi;
     }
