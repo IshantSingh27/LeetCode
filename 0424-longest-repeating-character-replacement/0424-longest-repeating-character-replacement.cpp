a5f5214cd020f6632acd88a8d7df475f2d428d8c
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-23 00:19:25 +0530

    Time: 0 ms (100%), Space: 10.8 MB (70.85%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -8,9 +8,9 @@ public:
             maxfreq = max(maxfreq , mp[s[r] - 'A']);
             if( (r - l + 1) - maxfreq > k){
                 mp[s[l] - 'A']--;
-                for(int i=0 ; i<26 ; i++) maxfreq = max(maxfreq , mp[i]);
-                l++;
-            }
+                // for(int i=0 ; i<26 ; i++) maxfreq = max(maxfreq , mp[i]);
+                l++; // no need to update max freq as we see the no. of different elemnet k is always taken
+            }// so in order to find better answer we need greater maxfreq so no need to update keep it like that
             else{
                 maxlen = max(maxlen , r - l + 1);
             }
