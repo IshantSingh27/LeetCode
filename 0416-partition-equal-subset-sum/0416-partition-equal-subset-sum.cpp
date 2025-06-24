581c450bf5551e2a7fca428f29c1ce8076f0780b
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-24 15:48:26 +0530

    Time: 139 ms (43.66%), Space: 110.4 MB (39.06%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    bool subsetSum(int n , int k , vector<int>& arr){
-        vector<bool> prev(k + 1 , false) , cur(k + 1 , false);
-        prev[0] = true;
-        cur[0] = true;
-        if(arr[0] <= k) prev[arr[0]] = true;
-        for(int i=1 ; i<n ; i++){
-            for(int tar=1 ; tar<=k ; tar++){
-                bool notTake = prev[tar];
-                bool take = false;
-                if(tar >= arr[i]){
-                    take = prev[tar - arr[i]];
-                }
-                cur[tar] = take || notTake;
-            }
-            prev = cur;
+    int solve(int ind , int tar , vector<int>& arr , vector<vector<int>>& dp){
+        if(tar == 0) return true;
+        if(ind == 0) return arr[0] == tar;
+
+        if(dp[ind][tar] != -1) return dp[ind][tar];
+
+        bool notTake = solve(ind - 1 , tar , arr , dp);
+        bool take = 0;
+        if(arr[ind] <= tar){
+            take = solve(ind - 1 , tar - arr[ind] , arr , dp);
         }
-        return prev[k];
+
+        return dp[ind][tar] = take || notTake;
     }
-    bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        for(auto it : nums){
-            sum += it;
+    bool canPartition(vector<int>& arr) {
+        int n = arr.size() , sum = 0;
+        for(int i=0 ; i<n ; i++){
+            sum += arr[i];
         }
         if(sum % 2 != 0) return false;
-        else{
-            return subsetSum(nums.size() , sum / 2 , nums);
-        }
+        vector<vector<int>> dp(n , vector<int>( (sum / 2) + 1 , -1 ));
+        return solve(n - 1 , sum / 2 , arr , dp);
     }
 };
