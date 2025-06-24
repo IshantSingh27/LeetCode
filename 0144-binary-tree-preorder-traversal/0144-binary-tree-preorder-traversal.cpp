6b97e1cbeed9357ba72033e3c19e0fcbcf6138e0
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-24 21:02:29 +0530

    Time: 0 ms (100%), Space: 11 MB (41.53%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -19,8 +19,19 @@ public:
         pre(root->right , ans);
     }
     vector<int> preorderTraversal(TreeNode* root) {
+        if(root == NULL) return {};
         vector<int> ans;
-        pre(root , ans);
+        // pre(root , ans);
+        // return ans;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* node = st.top();
+            st.pop();
+            ans.push_back(node->val);
+            if(node->right) st.push(node->right);
+            if(node->left) st.push(node->left);
+        }
         return ans;
     }
 };
