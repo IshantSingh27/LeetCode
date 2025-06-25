6074885639a5f2b44a09447334aacbcb9d98f46e
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-06-25 18:29:06 +0530

    Time: 43 ms (44.41%), Space: 48.9 MB (36.62%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -16,8 +16,26 @@ public:
     }
     int coinChange(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<vector<int>> dp(n , vector<int>(k + 1 , -1));
-        if(solve(n - 1 , k , arr , dp) == 1e9) return -1;
-        else return solve(n - 1 , k , arr , dp);
+        // vector<vector<int>> dp(n , vector<int>(k + 1 , -1));
+        // if(solve(n - 1 , k , arr , dp) == 1e9) return -1;
+        // else return solve(n - 1 , k , arr , dp);
+        vector<vector<int>> dp(n , vector<int>(k + 1 , 0));
+        for(int i=0 ; i <= k ; i++){
+            if(i % arr[0] == 0) dp[0][i] = i / arr[0];
+            else dp[0][i] = 1e9;
+        }
+
+        for(int ind = 1 ; ind < n ; ind++){
+            for(int tar=0 ; tar<=k ; tar++){
+                int notTake = 0 + dp[ind - 1][tar];
+                int take = 1e9;
+                if(arr[ind] <= tar){
+                    take = 1 + dp[ind][tar - arr[ind]];
+                }
+                dp[ind][tar] = min(take , notTake);
+            }
+        }
+        if(dp[n - 1][k] == 1e9) return -1;
+        else return dp[n - 1][k];
     }
 };
