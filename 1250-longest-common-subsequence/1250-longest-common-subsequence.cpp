518a0ca67b09fb953f52d5f4d78d78487e249f8a
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-05 15:52:11 +0530

    Time: 26 ms (63.3%), Space: 27.5 MB (50.64%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
-    // int solve(int i , int j , string s1, string s2 , vector<vector<int>>& dp){
-    //     if(i < 0 || j < 0) return 0;
-    //     if(dp[i][j] != -1) return dp[i][j];
-    //     if(s1[i] == s2[j]) return 1 + solve(i - 1 , j - 1 , s1 , s2 , dp);
-    //     int left = solve(i - 1 , j , s1 , s2 , dp);
-    //     int right = solve(i , j - 1 , s1 , s2 , dp);
-    //     return dp[i][j] = max(left , right);
-    // }
-    // int longestCommonSubsequence(string s1, string s2) {
-    //     int n = s1.size() , m = s2.size();
-    //     // vector<vector<int>> dp(n , vector<int>(m , -1));
-    //     // return solve(n - 1 , m - 1 , s1 , s2 , dp);
-    //     //TABULATION
-    //     vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , 0));
-    //     for(int i=0 ; i<=n ; i++) dp[i][0] = 0; // base case
-    //     for(int j=0 ; j<=m ; j++) dp[0][j] = 0; // rather of no use
-    //     for(int i=1 ; i<=n ; i++){
-    //         for(int j=1 ; j<=m ; j++){ //i - 1 , j - 1 for index shift
-    //             if(s1[i - 1] == s2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
-    //             else dp[i][j] = max(dp[i - 1][j] , dp[i][j - 1]);
-    //         }
-    //     }
-    //     return dp[n][m];
-    // }
-    //SPACE OPT
+    int solve(int ind1, int ind2, string s1, string s2, vector<vector<int>>& dp){
+        if(ind1 < 0 || ind2 < 0) return 0;
+
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+
+        if(s1[ind1] == s2[ind2]) return 1 + solve(ind1 - 1 , ind2 - 1 , s1 , s2 , dp);
+
+        return dp[ind1][ind2] = 0 + max(solve(ind1 - 1, ind2, s1, s2 , dp) , solve(ind1, ind2 - 1, s1, s2, dp));
+    }
+
     int longestCommonSubsequence(string s1, string s2) {
         int n = s1.size() , m = s2.size();
-        vector<int> prev(m + 1) , cur(m + 1 , 0);
-        for(int i=0 ; i<=m ; i++) prev[i] = 0;
-        for(int i=1 ; i<=n ; i++){
-            for(int j=1 ; j<=m ; j++){ //i - 1 , j - 1 for index shift
-                if(s1[i - 1] == s2[j - 1]) cur[j] = 1 + prev[j - 1];
-                else cur[j] = max(prev[j] , cur[j - 1]);
+        // vector<vector<int>> dp(n , vector<int>(m , -1));
+        // return solve(n - 1 , m - 1 , s1 , s2 , dp);
+
+        vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , 0));
+
+        for(int ind1 = 1 ; ind1 <= n ; ind1++){
+            for(int ind2 = 1 ; ind2 <= m ; ind2++){
+                if(s1[ind1 - 1] == s2[ind2 - 1]) dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+
+                else dp[ind1][ind2] = 0 + max(dp[ind1 - 1][ind2] , dp[ind1][ind2 - 1]);
             }
-            prev = cur;
         }
-        return prev[m];
+
+        return dp[n][m];
     }
 };
