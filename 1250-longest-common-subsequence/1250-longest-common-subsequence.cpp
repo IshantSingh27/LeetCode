7af49721ca3b24e4c625a8fe9dfdbcaaad7f917d
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-05 15:53:57 +0530

    Time: 15 ms (91.38%), Space: 9.1 MB (96.33%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -10,21 +10,38 @@ public:
         return dp[ind1][ind2] = 0 + max(solve(ind1 - 1, ind2, s1, s2 , dp) , solve(ind1, ind2 - 1, s1, s2, dp));
     }
 
+    // int longestCommonSubsequence(string s1, string s2) {
+    //     int n = s1.size() , m = s2.size();
+    //     // vector<vector<int>> dp(n , vector<int>(m , -1));
+    //     // return solve(n - 1 , m - 1 , s1 , s2 , dp);
+
+    //     vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , 0));
+
+    //     for(int ind1 = 1 ; ind1 <= n ; ind1++){
+    //         for(int ind2 = 1 ; ind2 <= m ; ind2++){
+    //             if(s1[ind1 - 1] == s2[ind2 - 1]) dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+
+    //             else dp[ind1][ind2] = 0 + max(dp[ind1 - 1][ind2] , dp[ind1][ind2 - 1]);
+    //         }
+    //     }
+
+    //     return dp[n][m];
+    // }
+
     int longestCommonSubsequence(string s1, string s2) {
         int n = s1.size() , m = s2.size();
-        // vector<vector<int>> dp(n , vector<int>(m , -1));
-        // return solve(n - 1 , m - 1 , s1 , s2 , dp);
 
-        vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , 0));
+        vector<int> prev(m + 1 , 0) , cur(m + 1 , 0);
 
         for(int ind1 = 1 ; ind1 <= n ; ind1++){
             for(int ind2 = 1 ; ind2 <= m ; ind2++){
-                if(s1[ind1 - 1] == s2[ind2 - 1]) dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                if(s1[ind1 - 1] == s2[ind2 - 1]) cur[ind2] = 1 + prev[ind2 - 1];
 
-                else dp[ind1][ind2] = 0 + max(dp[ind1 - 1][ind2] , dp[ind1][ind2 - 1]);
+                else cur[ind2] = 0 + max(prev[ind2] , cur[ind2 - 1]);
             }
+            prev = cur;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
