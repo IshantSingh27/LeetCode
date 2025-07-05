92f9356dc76c8af4df2628f2e0df81f646e2e72e
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-05 17:15:03 +0530

    Time: 3 ms (97.52%), Space: 11.7 MB (84.63%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -3,16 +3,23 @@ public:
     int solve(string s1 , string s2){
         int n = s1.size() , m = s2.size();
         vector<int> prev(m + 1 , 0) , cur(m + 1 , 0);
-        for(int i=1 ; i<=n ; i++){
-            for(int j=1  ; j<=m ; j++){
-                if(s1[i - 1] == s2[j - 1]) cur[j] = 1 + prev[j - 1];
-                else cur[j] = max(cur[j - 1] , prev[j]);
+
+        for(int ind1 = 1 ; ind1 <= n ; ind1++){
+            for(int ind2 = 1 ; ind2 <= m ; ind2++){
+                if(s1[ind1 - 1] == s2[ind2 - 1]){
+                    cur[ind2] = 1 + prev[ind2 - 1];
+                }
+                else{
+                    cur[ind2] = 0 + max(cur[ind2 - 1] , prev[ind2]);
+                }
             }
             prev = cur;
         }
+
         return prev[m];
     }
+
     int minDistance(string s1, string s2) {
-        return s1.size() + s2.size() - 2 * solve(s1 , s2);
+        return s1.size() + s2.size() - (2 * solve(s1 , s2));
     }
 };
