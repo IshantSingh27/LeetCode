4ec29cc36d8048465d5b94e11ea977a6b2af526b
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-06 12:24:48 +0530

    Time: 14 ms (85.27%), Space: 9 MB (86.25%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -7,9 +7,34 @@ public:
         if(s[i] == t[j]) return dp[i][j] = solve(i-1, j-1, s, t, dp) + solve(i-1, j, s, t, dp);
         else return dp[i][j] = solve(i-1, j, s, t, dp);
     }
+    // int numDistinct(string s, string t) {
+    //     long long n = s.size() , m = t.size();
+    //     // vector<vector<int>> dp(n , vector<int>(m , -1));
+    //     // return solve(n - 1 , m - 1 , s , t , dp);
+    //     //TABULATION
+    //     vector<vector<long long>> dp(n + 1 , vector<long long>(m + 1 , 0));
+    //     for(long long i=0 ; i<=n ; i++) dp[i][0] = 1;
+    //     for(long long i=1 ; i<=n ; i++){
+    //         for(long long j=1 ; j<=m ; j++){
+    //             if(s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+    //             else dp[i][j] = dp[i-1][j];
+    //         }
+    //     }
+    //     return dp[n][m];
+    // }
+    //SPACE OPTIMISATION
     int numDistinct(string s, string t) {
-        int n = s.size() , m = t.size();
-        vector<vector<int>> dp(n , vector<int>(m , -1));
-        return solve(n - 1 , m - 1 , s , t , dp);
+        unsigned long long n = s.size() , m = t.size();
+        vector<unsigned long long> prev(m + 1 , 0) , cur(m + 1 , 0);
+        prev[0] = 1;
+        cur[0] = 1;
+        for(unsigned long long i=1 ; i<=n ; i++){
+            for(unsigned long long j=1 ; j<=m ; j++){
+                if(s[i-1] == t[j-1]) cur[j] = prev[j-1] + prev[j];
+                else cur[j] = prev[j];
+            }
+            prev = cur;
+        }
+        return prev[m];
     }
 }; 
