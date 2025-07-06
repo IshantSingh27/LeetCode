38ee8bad700ec16159b0e100f7c21b37e30a1789
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-06 21:52:06 +0530

    Time: 17 ms (93.09%), Space: 10.1 MB (93.63%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -14,31 +14,54 @@ public:
         else if(p[i] == '*') return dp[i][j] = solve(i, j - 1, p, s, dp) || solve(i - 1, j, p, s, dp);
         else return dp[i][j] = false;
     }
+    // bool isMatch(string s, string p) {
+    //     int n = p.size() , m = s.size();
+    //     // vector<vector<int>> dp(n , vector<int>(m , -1));
+    //     // return solve(n - 1 , m - 1 , p , s , dp);
+    //     //TABULATION
+    //     vector<vector<bool>> dp(n + 1 , vector<bool>(m + 1 , false));
+    //     dp[0][0] = true; //s1 khatam ho gya s2 bach gya false hoga wo
+    //     for(int j=1 ; j<=m ; j++) dp[0][j] = false; // by default hai
+    //     for (int i = 1; i <= n; i++) {
+    //         bool chk = true;
+    //         for (int k = 1; k <= i; k++) {
+    //             if (p[k - 1] != '*') {
+    //             chk = false;
+    //             break;
+    //             }
+    //         }
+    //         dp[i][0] = chk;
+    //     }
+    //     for(int i=1 ; i<=n ; i++){
+    //         for(int j=1 ; j<=m ; j++){
+    //             if((p[i - 1] == s[j - 1]) || p[i - 1] == '?') dp[i][j] = dp[i - 1][j - 1];
+    //             else if(p[i - 1] == '*') dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+    //             else dp[i][j] = false;
+    //         }
+    //     }
+    //     return dp[n][m];
+    // }
+    //SPACE OPTIMISATION
     bool isMatch(string s, string p) {
         int n = p.size() , m = s.size();
-        // vector<vector<int>> dp(n , vector<int>(m , -1));
-        // return solve(n - 1 , m - 1 , p , s , dp);
-        //TABULATION
-        vector<vector<bool>> dp(n + 1 , vector<bool>(m + 1 , false));
-        dp[0][0] = true;
-        for (int i = 1; i <= n; i++) {
+        vector<bool> prev(m + 1 , false) , cur(m + 1 , false);
+        prev[0] = true; //s1 khatam ho gya s2 bach gya false hoga wo by default hai
+        for(int i=1 ; i<=n ; i++){
             bool chk = true;
             for (int k = 1; k <= i; k++) {
-                if (p[k - 1] != '*') {
-                chk = false;
-                break;
+                    if (p[k - 1] != '*') {
+                    chk = false;
+                    break;
                 }
             }
-            dp[i][0] = chk;
-        }
-
-        for(int i=1 ; i<=n ; i++){
+            cur[0] = chk;
             for(int j=1 ; j<=m ; j++){
-                if((p[i - 1] == s[j - 1]) || p[i - 1] == '?') dp[i][j] = dp[i - 1][j - 1];
-                else if(p[i - 1] == '*') dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                else dp[i][j] = false;
+                if((p[i - 1] == s[j - 1]) || p[i - 1] == '?') cur[j] = prev[j - 1];
+                else if(p[i - 1] == '*') cur[j] = prev[j] || cur[j - 1];
+                else cur[j] = false;
             }
+            prev = cur;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
