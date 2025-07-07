8b9919ee8cdae90374453ec5b04f8f03a39e8868
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-07 23:25:12 +0530

    Time: 37 ms (37.54%), Space: 14 MB (24.52%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -2,34 +2,24 @@ class Solution {
 public:
     int solve(int i , int j , vector<int>& arr , vector<vector<int>>& dp){
         if(i > j) return 0;
+
         if(dp[i][j] != -1) return dp[i][j];
+
         int mini = 1e9;
         for(int ind = i ; ind <= j ; ind++){
-            int cost = arr[j + 1] - arr[i - 1] + solve(i , ind - 1 , arr , dp) + solve(ind + 1 , j , arr , dp);
+            int cost = arr[j + 1] - arr[i - 1] + solve(i, ind - 1, arr, dp) + solve(ind + 1, j, arr, dp);
             mini = min(mini , cost);
         }
+
         return dp[i][j] = mini;
     }
+
     int minCost(int n, vector<int>& arr) {
-        arr.push_back(n);
         arr.insert(arr.begin() , 0);
+        arr.push_back(n);
         sort(arr.begin() , arr.end());
-        int m = arr.size();
-        // vector<vector<int>> dp(m , vector<int>(m , -1));
-        // return solve(1 , m - 2 , arr , dp);
-        //TABULATION
-        vector<vector<int>> dp(m , vector<int>(m , 0)); //base case 0 hi karna hai
-        for(int i = m - 2 ; i > 0 ; i--){
-            for(int j = 1 ; j <= m - 2 ; j++){ // obvio reverse se start hoga
-            if(i > j) continue;
-                int mini = 1e9;
-                for(int ind = i ; ind <= j ; ind++){
-                    int cost = arr[j + 1] - arr[i - 1] + dp[ind + 1][j] + dp[i][ind - 1];
-                    mini = min(mini , cost);
-                }
-                dp[i][j] = mini;
-            }
-        }
-        return dp[1][m - 2];
+        int sz = arr.size();
+        vector<vector<int>> dp(sz , vector<int>(sz , -1));
+        return solve(1 , sz - 2 , arr , dp);
     }
 };
