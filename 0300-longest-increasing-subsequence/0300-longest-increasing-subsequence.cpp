7f85804fb4096f788e3a6b5b30d44c25341461e3
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-09 00:06:57 +0530

    Time: 139 ms (44.18%), Space: 14.6 MB (44.46%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -13,23 +13,41 @@ public:
         return dp[ind][prev + 1] = max(take , notTake);
     }
 
+    // int lengthOfLIS(vector<int>& arr) {
+    //     int n = arr.size();
+    //     // vector<vector<int>> dp(n , vector<int>(n + 1 , -1));
+    //     // return solve(0 , -1 , arr , dp);
+
+    //     vector<vector<int>> dp(n + 1 , vector<int>(n + 1 , 0));
+
+    //     for(int ind = n-1 ; ind >= 0 ; ind--){
+    //         for(int prev = ind-1 ; prev >= -1 ; prev--){
+    //             int notTake = 0 + dp[ind + 1][prev + 1];
+    //             int take = 0;
+    //             if(prev == -1 || arr[prev] < arr[ind]){
+    //                 take = 1 + dp[ind + 1][ind + 1];
+    //             }
+    //             dp[ind][prev + 1] = max(take , notTake);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
     int lengthOfLIS(vector<int>& arr) {
         int n = arr.size();
-        // vector<vector<int>> dp(n , vector<int>(n + 1 , -1));
-        // return solve(0 , -1 , arr , dp);
 
-        vector<vector<int>> dp(n + 1 , vector<int>(n + 1 , 0));
+        vector<int> next(n + 1 , 0) , cur(n + 1 , 0);
 
         for(int ind = n-1 ; ind >= 0 ; ind--){
             for(int prev = ind-1 ; prev >= -1 ; prev--){
-                int notTake = 0 + dp[ind + 1][prev + 1];
+                int notTake = 0 + next[prev + 1];
                 int take = 0;
                 if(prev == -1 || arr[prev] < arr[ind]){
-                    take = 1 + dp[ind + 1][ind + 1];
+                    take = 1 + next[ind + 1];
                 }
-                dp[ind][prev + 1] = max(take , notTake);
+                cur[prev + 1] = max(take , notTake);
             }
+            next = cur;
         }
-        return dp[0][0];
+        return next[0];
     }
 };
