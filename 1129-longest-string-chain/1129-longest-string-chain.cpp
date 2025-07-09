29178503f5836e112e8249ec72783287ee64a897
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-09 10:42:01 +0530

    Time: 1855 ms (17.83%), Space: 652.5 MB (5.42%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-    int check(string s1 , string s2){
-        if(s1.size() != 1 + s2.size()) return false;
+    bool solve(string s1 , string s2){
+        if(s1.size() != s2.size() + 1) return false;
+        
         int first = 0 , second = 0;
-        while(first < s1.size() && second < s2.size()){
+        while(first < s1.size()){
             if(s1[first] == s2[second]){
                 first++;
                 second++;
             }
-            else{
-                first++;
-            }
+            else first++;
         }
-        if(second != s2.size()) return false;
-        else return true;
+
+        return second == s2.size();
     }
     static bool comp(string s1 , string s2){
         return s1.size() < s2.size();
     }
     int longestStrChain(vector<string>& arr) {
         sort(arr.begin() , arr.end() , comp);
-        int n = arr.size() , maxi = 0;
+
+        int n = arr.size() , ans = 0;
         vector<int> dp(n , 1);
-        for(int ind = 0 ; ind < n ; ind++){
-            for(int prev = 0 ; prev < ind ; prev++){
-                if(check(arr[ind] , arr[prev]) && dp[ind] < dp[prev] + 1){
-                    dp[ind] = dp[prev] + 1;
+
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<=i ; j++){
+                if(solve(arr[i] , arr[j]) && dp[i] < dp[j] + 1){
+                    dp[i] = dp[j] + 1;
                 }
             }
-            maxi = max(dp[ind] , maxi);
+            ans = max(ans , dp[i]);
         }
-        return maxi;
+
+        return ans;
     }
 };
