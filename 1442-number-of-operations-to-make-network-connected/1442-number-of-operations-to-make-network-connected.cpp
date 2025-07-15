06964f30e962efbb9d48cddebbfb861cd0760ac3
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-15 10:38:45 +0530

    Time: 22 ms (44.82%), Space: 46.8 MB (40.77%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,45 +1,23 @@
-class disjointSet{
-public:
-    vector<int> rank , parent , size;
-
-    disjointSet(int n){
-        rank.resize(n + 1 , 0);
-        parent.resize(n+1);
-        size.resize(n+1);
-        for(int i=0 ; i<=n ; i++){
-            parent[i] = i;
-            size[i] = 1;
-        }
+class disjoint{
+  public:
+    vector<int> size , parent;
+    
+    disjoint(int n){
+        size.resize(n + 1 , 1);
+        parent.resize(n + 1 , 0);
+    
+        for(int i=0 ; i<n ; i++) parent[i] = i;
     }
-
+    
     int find(int x){
-        if(x == parent[x]) return x;
-
-        return parent[x] = find(parent[x]);
+        if(parent[x] == x) return x;
+        else return parent[x] = find(parent[x]);
     }
-
-    void unionByRank(int x , int y){
-        int px = find(x);
-        int py = find(y);
-
-        if(px == py) return;
-        if(rank[px] < rank[py]){
-            parent[px] = py;
-        }
-        else if(rank[py] < rank[px]){
-            parent[py] = px;
-        }
-        else{
-            parent[py] = px;
-            rank[px]++;
-        }
-    }
-
+    
     void unionBySize(int x , int y){
         int px = find(x);
         int py = find(y);
-
-        if(px == py) return;
+        if(parent[px] == parent[py]) return;
         if(size[px] < size[py]){
             parent[px] = py;
             size[py] += size[px];
@@ -50,34 +28,23 @@ public:
         }
     }
 };
-
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& arr) {
-        disjointSet ds(n);
-
-        int cntex = 0;
+        disjoint ds(n);
+        int cnte = 0 , cntc = 0;
         for(auto it : arr){
             int u = it[0];
             int v = it[1];
-            if(ds.find(u) == ds.find(v)){
-                cntex++;
-            }
-            else{
-                ds.unionBySize(u , v);
-            }
+            if(ds.find(u) == ds.find(v)) cnte++;
+            else ds.unionBySize(u , v);
         }
 
-        int ans = 0;
         for(int i=0 ; i<n ; i++){
-            if(ds.parent[i] == i){
-                ans++;
-            }
+            if(ds.parent[i] == i) cntc++;
         }
 
-        if(ans - 1 <= cntex){
-            return ans - 1;
-        }
+        if(cnte >= cntc - 1) return cntc - 1;
         else return -1;
     }
 };
