085c94e275feaa4288b8212bab56b21711b75b9c
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-16 18:18:44 +0530

    Time: 617 ms (11.98%), Space: 225.2 MB (12.29%) - LeetHub

---
+++ 0854-making-a-large-island/0854-making-a-large-island.cpp
@@ -0,0 +1,90 @@
+class disjoint{
+  public:
+    vector<int> size , parent;
+    
+    disjoint(int n){
+        size.resize(n + 1 , 1);
+        parent.resize(n + 1 , 0);
+    
+        for(int i=0 ; i<n ; i++) parent[i] = i;
+    }
+    
+    int find(int x){
+        if(parent[x] == x) return x;
+        else return parent[x] = find(parent[x]);
+    }
+    
+    void unionBySize(int x , int y){
+        int px = find(x);
+        int py = find(y);
+        if(parent[px] == parent[py]) return;
+        if(size[px] < size[py]){
+            parent[px] = py;
+            size[py] += size[px];
+        }
+        else{
+            parent[py] = px;
+            size[px] += size[py];
+        }
+    }
+};
+class Solution {
+public:
+    int largestIsland(vector<vector<int>>& arr) {
+        int n = arr.size() , ans = 0;
+        disjoint ds(n * n);
+        for(int row=0 ; row<n ; row++){
+            for(int col=0 ; col<n ; col++){
+                if(arr[row][col] == 0) continue;
+                
+                vector<int> drow = {-1 , 0 , 1 , 0};
+                vector<int> dcol = {0 , 1 , 0 , -1};
+
+                for(int i=0 ; i<4 ; i++){
+                    int nrow = row + drow[i];
+                    int ncol = col + dcol[i];
+
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && arr[nrow][ncol] == 1){
+                        int num = (row * n) + col;
+                        int adjnum = (nrow * n) + ncol;
+
+                        ds.unionBySize(num , adjnum);
+                    }
+                }
+            }
+        }
+
+        for(int row=0 ; row<n ; row++){
+            for(int col=0 ; col<n ; col++){
+                if(arr[row][col] == 1) continue;
+                
+                vector<int> drow = {-1 , 0 , 1 , 0};
+                vector<int> dcol = {0 , 1 , 0 , -1};
+
+                set<int> sz;
+                for(int i=0 ; i<4 ; i++){
+                    int nrow = row + drow[i];
+                    int ncol = col + dcol[i];
+
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && arr[nrow][ncol] == 1){
+                        int adjnum = (nrow * n) + ncol;
+                        sz.insert(ds.find(adjnum));
+                    }
+                }
+
+                int maxi = 0;
+                for(auto it : sz){
+                    maxi += ds.size[it];
+                }
+
+                ans = max(ans , maxi + 1);
+            }
+        }
+
+        for(int i=0 ; i<(n * n) ; i++){
+            ans = max(ans , ds.size[ds.find(i)]);
+        }
+
+        return ans;
+    }
+};
