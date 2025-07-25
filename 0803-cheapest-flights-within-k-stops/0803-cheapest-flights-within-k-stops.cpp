20272e920202ef30dbdbba50852e0bd9656e05a2
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-25 16:12:43 +0530

    Time: 7 ms (27.14%), Space: 18.3 MB (40.04%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -5,26 +5,31 @@ public:
         for(auto it : flights){
             adj[it[0]].push_back({it[1] , it[2]});
         }
+
         queue<pair<int , pair<int , int>>> q;
         q.push({0 , {src , 0}});
-        vector<int> cost(n , 1e9);
-        cost[src] = 0;
+        vector<int> dist(n , 1e9);
+        dist[src] = 0;
+
         while(!q.empty()){
             int stops = q.front().first;
             int node = q.front().second.first;
-            int price = q.front().second.second;
+            int cost = q.front().second.second;
             q.pop();
+
             if(stops > k) continue;
             for(auto it : adj[node]){
                 int adjnode = it.first;
-                int adjprice = it.second;
-                if(price + adjprice < cost[adjnode]){
-                    cost[adjnode] = price + adjprice;
-                    q.push({stops + 1 , {adjnode , cost[adjnode]}});
+                int adjcost = it.second;
+
+                if(cost + adjcost < dist[adjnode]){
+                    dist[adjnode] = cost + adjcost;
+                    q.push({stops + 1 , {adjnode , dist[adjnode]}});
                 }
             }
         }
-        if(cost[dst] == 1e9) return -1;
-        else return cost[dst];
+
+        if(dist[dst] == 1e9) return -1;
+        else return dist[dst];
     }
 };
