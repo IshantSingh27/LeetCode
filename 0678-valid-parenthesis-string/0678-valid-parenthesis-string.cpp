59935db163205850341c9334540e50f8590c3fea
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-28 19:06:50 +0530

    Time: 0 ms (100%), Space: 8 MB (92.95%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int min = 0 , max = 0 , n = s.size();
+
+        for(int i=0 ; i<n ; i++){
+            if(s[i] == '('){
+                min++;
+                max++;
+            }
+            else if(s[i] == ')'){
+                min--;
+                max--;
+            }
+            else{
+                min--;
+                max++;
+            }
+            if(min < 0) min = 0;
+            if(max < 0) return false;
+        }
+
+        if(min == 0) return true;
+        else return false;
+    }
+};
