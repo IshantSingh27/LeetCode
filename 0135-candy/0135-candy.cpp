e49302fbe6ce3c5c4c25890dbd293c93a6c67801
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-28 23:03:00 +0530

    Time: 0 ms (100%), Space: 22.8 MB (80.05%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -32,19 +32,24 @@ class Solution {
 public:
     int candy(vector<int>& arr) {
         int n = arr.size();
-        vector<int> left(n , 1) , right(n , 1);
+        vector<int> left(n , 1);
 
         for(int i=1 ; i<n ; i++){
             if(arr[i] > arr[i - 1]) left[i] = left[i - 1] + 1;
         }
 
+        int cur = 1 , right = 1 , sum = max(left[n - 1] , 1);
         for(int i=n-2 ; i>=0 ; i--){
-            if(arr[i] > arr[i + 1]) right[i] = right[i + 1] + 1;
-        }
+            if(arr[i] > arr[i + 1]){
+                cur = right + 1;
+                right = cur;
+            }
+            else{
+                right = 1;
+                cur = 1;
+            }
 
-        int sum = 0;
-        for(int i=0 ; i<n ; i++){
-            sum += max(left[i] , right[i]);
+            sum += max(left[i] , cur);
         }
 
         return sum;
