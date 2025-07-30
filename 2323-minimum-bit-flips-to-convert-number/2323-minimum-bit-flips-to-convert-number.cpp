952fd2dcf6e7a7f7cfacb0efc27f9af3e71a112e
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-30 18:19:00 +0530

    Time: 0 ms (100%), Space: 8 MB (60.14%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -4,10 +4,14 @@ public:
         int num = s ^ g;
 
         int cnt = 0;
-        for(int i=0 ; i<=31 ; i++){
-            if( (num & (1 << i)) != 0){
-                cnt++;
-            }
+        // for(int i=0 ; i<=31 ; i++){
+        //     if( (num & (1 << i)) != 0){
+        //         cnt++;
+        //     }
+        // }
+        while(num > 0){
+            if(num % 2 == 1) cnt++;
+            num = num / 2;
         }
 
         return cnt;
