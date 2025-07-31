332805e1029de3a2fa330cd14d4f5ca0fb9e7110
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-07-31 22:25:31 +0530

    Time: 0 ms (100%), Space: 13.4 MB (64.41%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,21 +1,35 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& arr) {
+//         int ans = 0  , n = arr.size();
+
+//         for(int i=0 ; i<=31 ; i++){
+//             int cnt = 0;
+//             for(int j=0 ; j<n ; j++){
+//                 if( (arr[j] & (1 << i)) != 0 ){
+//                     cnt++;
+//                 }
+//             }
+
+//             if(cnt % 3 != 0){
+//                 ans = (ans | (1 << i));
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int singleNumber(vector<int>& arr) {
-        int ans = 0  , n = arr.size();
+        int n = arr.size();
+        if(n == 1) return arr[0];
+        sort(arr.begin() , arr.end());
 
-        for(int i=0 ; i<=31 ; i++){
-            int cnt = 0;
-            for(int j=0 ; j<n ; j++){
-                if( (arr[j] & (1 << i)) != 0 ){
-                    cnt++;
-                }
-            }
-
-            if(cnt % 3 != 0){
-                ans = (ans | (1 << i));
-            }
+        for(int i=1 ; i<n ; i += 3){
+            if(arr[i] != arr[i - 1]) return arr[i - 1];
         }
 
-        return ans;
+        return arr[n - 1];
     }
 };
