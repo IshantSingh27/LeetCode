ce1ef6f160f7e2fbd3e9d48f47914e89fb74f51c
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-08-05 01:47:58 +0530

    Time: 3 ms (86.29%), Space: 14.4 MB (88.69%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,9 +3,20 @@ public:
     int maxProfit(int k, vector<int>& arr) {
         int n = arr.size();
         vector<vector<int>> prev(2 , vector<int>(k + 1 , 0)) , cur(2 , vector<int>(k + 1 , 0));
-        for(int ind = n - 1 ; ind >= 0 ; ind--){
-            for(int buy = 0 ; buy <= 1 ; buy++){
-                for(int cap = 1 ; cap <= k ; cap++){
+
+        // for(int j=0 ; j<=1 ; j++){
+        //     prev[j][0] = 0;
+        //     cur[j][0] = 0;
+        // }
+        // for(int i=0 ; i<=1 ; i++){
+        //     for(int j=0 ; j<=k ; j++){
+        //         prev[i][j] = 0;
+        //     }
+        // }
+
+        for(int ind=n-1 ; ind>=0 ; ind--){
+            for(int buy=0 ; buy<=1 ; buy++){
+                for(int cap=1 ; cap<=k ; cap++){
                     int profit = 0;
                     if(buy){
                         profit = max(prev[0][cap] - arr[ind] , prev[1][cap] - 0);
@@ -13,11 +24,13 @@ public:
                     else{
                         profit = max(prev[1][cap - 1] + arr[ind] , prev[0][cap] + 0);
                     }
+
                     cur[buy][cap] = profit;
                 }
             }
             prev = cur;
         }
+
         return prev[1][k];
     }
 };
