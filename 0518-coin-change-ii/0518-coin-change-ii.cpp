528ba52a62f87b3bcbf3a9562263b128a4433d81
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-08-06 17:59:36 +0530

    Time: 16 ms (65.33%), Space: 28.3 MB (62.85%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,62 +1,24 @@
 class Solution {
 public:
-    int solve(int ind , int tar , vector<int>& arr , vector<vector<int>>& dp){
+    int solve(int ind , int amt , vector<int>& arr , vector<vector<int>>& dp){
         if(ind == 0){
-            if(tar % arr[0] == 0) return 1;
+            if(amt % arr[0] == 0) return 1;
             else return 0;
         }
 
-        if(dp[ind][tar] != -1) return dp[ind][tar];
+        if(dp[ind][amt] != -1) return dp[ind][amt];
 
-        int notTake = solve(ind - 1 , tar , arr , dp);
+        int notTake = solve(ind - 1 , amt , arr , dp);
         int take = 0;
-        if(arr[ind] <= tar){
-            take = solve(ind , tar - arr[ind] , arr , dp);
+        if(arr[ind] <= amt){
+            take = solve(ind , amt - arr[ind] , arr , dp);
         }
 
-        return dp[ind][tar] = take + notTake;
+        return dp[ind][amt] = take + notTake;
     }
-    // int change(int k, vector<int>& arr) {
-    //     int n = arr.size();
-    //     // vector<vector<int>> dp(n , vector<int>(k + 1 , -1));
-    //     // return solve(n - 1 , k , arr , dp);
-    //     vector<vector<long long>> dp(n , vector<long long>(k + 1 , 0));
-    //     for(long long i=0 ; i<=k ; i++){
-    //         if(i % arr[0] == 0) dp[0][i] = 1;
-    //     }
-    //     long long mod = 1e12 + 7;
-
-    //     for(long long ind = 1 ; ind < n ; ind++){
-    //         for(long long tar = 0 ; tar <= k ; tar++){
-    //             long long notTake = dp[ind - 1][tar];
-    //             long long take = 0;
-    //             if(arr[ind] <= tar){
-    //                 take = dp[ind][tar - arr[ind]];
-    //             }
-    //             dp[ind][tar] = (take + notTake) % mod;
-    //         }
-    //     }
-    //     return dp[n - 1][k];
-    // }
-    int change(int k, vector<int>& arr) {
+    int change(int amt, vector<int>& arr) {
         int n = arr.size();
-        vector<long long> prev(k + 1 , 0) , cur(k + 1 , 0);
-        for(long long i=0 ; i<=k ; i++){
-            if(i % arr[0] == 0) prev[i] = 1;
-        }
-        long long mod = 1e12 + 7;
-
-        for(long long ind = 1 ; ind < n ; ind++){
-            for(long long tar = 0 ; tar <= k ; tar++){
-                long long notTake = prev[tar];
-                long long take = 0;
-                if(arr[ind] <= tar){
-                    take = cur[tar - arr[ind]];
-                }
-                cur[tar] = (take + notTake) % mod;
-            }
-            prev = cur;
-        }
-        return prev[k];
+        vector<vector<int>> dp(n , vector<int>(amt + 1 , -1));
+        return solve(n - 1 , amt , arr , dp);
     }
 };
