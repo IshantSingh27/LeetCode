25a35a18322134710fff44dd0595ea83bd46cafb
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-08-06 11:33:00 +0530

    Time: 48 ms (31.39%), Space: 49.4 MB (27.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,62 +1,26 @@
 class Solution {
 public:
-    int solve(int ind , int tar , vector<int>& arr , vector<vector<int>>& dp){
+    int solve(int ind , int amt , vector<int>& arr , vector<vector<int>>& dp){
         if(ind == 0){
-            if(tar % arr[0] == 0) return tar / arr[0];
+            if(amt % arr[0] == 0) return amt / arr[0];
             else return 1e9;
         }
 
-        if(dp[ind][tar] != -1) return dp[ind][tar];
+        if(dp[ind][amt] != -1) return dp[ind][amt];
 
-        int notTake = 0 + solve(ind - 1 , tar , arr , dp);
+        int notTake = 0 + solve(ind - 1 , amt , arr , dp);
         int take = 1e9;
-        if(arr[ind] <= tar) take = 1 + solve(ind , tar - arr[ind] , arr , dp);
+        if(arr[ind] <= amt){
+            take = 1 + solve(ind , amt - arr[ind] , arr , dp);
+        }
 
-        return dp[ind][tar] = min(take , notTake);
+        return dp[ind][amt] = min(take , notTake);
     }
-    // int coinChange(vector<int>& arr, int k) {
-    //     int n = arr.size();
-    //     // vector<vector<int>> dp(n , vector<int>(k + 1 , -1));
-    //     // if(solve(n - 1 , k , arr , dp) == 1e9) return -1;
-    //     // else return solve(n - 1 , k , arr , dp);
-    //     vector<vector<int>> dp(n , vector<int>(k + 1 , 0));
-    //     for(int i=0 ; i <= k ; i++){
-    //         if(i % arr[0] == 0) dp[0][i] = i / arr[0];
-    //         else dp[0][i] = 1e9;
-    //     }
-
-    //     for(int ind = 1 ; ind < n ; ind++){
-    //         for(int tar=0 ; tar<=k ; tar++){
-    //             int notTake = 0 + dp[ind - 1][tar];
-    //             int take = 1e9;
-    //             if(arr[ind] <= tar){
-    //                 take = 1 + dp[ind][tar - arr[ind]];
-    //             }
-    //             dp[ind][tar] = min(take , notTake);
-    //         }
-    //     }
-    //     if(dp[n - 1][k] == 1e9) return -1;
-    //     else return dp[n - 1][k];
-    // }
-    int coinChange(vector<int>& arr, int k) {
+    int coinChange(vector<int>& arr, int amt) {
         int n = arr.size();
-        vector<int> prev(k + 1 , 0);
-        for(int i=0 ; i <= k ; i++){
-            if(i % arr[0] == 0) prev[i] = i / arr[0];
-            else prev[i] = 1e9;
-        }
-
-        for(int ind = 1 ; ind < n ; ind++){
-            for(int tar=0 ; tar<=k ; tar++){
-                int notTake = 0 + prev[tar];
-                int take = 1e9;
-                if(arr[ind] <= tar){
-                    take = 1 + prev[tar - arr[ind]];
-                }
-                prev[tar] = min(take , notTake);
-            }
-        }
-        if(prev[k] == 1e9) return -1;
-        else return prev[k];
+        vector<vector<int>> dp(n , vector<int>(amt + 1 ,-1));
+        int ans = solve(n - 1 , amt , arr , dp);
+        if(ans == 1e9) return -1;
+        else return ans;
     }
 };
