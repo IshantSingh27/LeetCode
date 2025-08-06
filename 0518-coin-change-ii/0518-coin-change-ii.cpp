827a47a917a7fd4dca2b239eb5e2b34d483ab161
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-08-06 18:07:26 +0530

    Time: 27 ms (36.47%), Space: 46.2 MB (14.62%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -18,7 +18,26 @@ public:
     }
     int change(int amt, vector<int>& arr) {
         int n = arr.size();
-        vector<vector<int>> dp(n , vector<int>(amt + 1 , -1));
-        return solve(n - 1 , amt , arr , dp);
+        // vector<vector<int>> dp(n , vector<int>(amt + 1 , -1));
+        // return solve(n - 1 , amt , arr , dp);
+
+        long long mod = 1e18 + 7;
+        vector<vector<long long>> dp(n , vector<long long>(amt + 1 , 0));
+
+        for(long long i=0 ; i<=amt ; i++){
+            if(i % arr[0] == 0) dp[0][i] = 1;
+        }
+
+        for(long long ind=1 ; ind<n ; ind++){
+            for(long long w=0 ; w<=amt ; w++){
+                long long notTake = dp[ind - 1][w];
+                long long take = 0;
+                if(arr[ind] <= w) take = dp[ind][w - arr[ind]];
+
+                dp[ind][w] = (take + notTake) % mod;
+            }
+        }
+
+        return dp[n - 1][amt];
     }
 };
