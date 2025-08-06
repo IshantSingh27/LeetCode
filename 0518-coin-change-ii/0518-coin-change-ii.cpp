b9168ba95357acc5a98dd877b2753af6d1e47926
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-08-06 18:09:06 +0530

    Time: 21 ms (47.43%), Space: 10.8 MB (76.12%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -16,28 +16,51 @@ public:
 
         return dp[ind][amt] = take + notTake;
     }
+    // int change(int amt, vector<int>& arr) {
+    //     int n = arr.size();
+    //     // vector<vector<int>> dp(n , vector<int>(amt + 1 , -1));
+    //     // return solve(n - 1 , amt , arr , dp);
+
+    //     long long mod = 1e18 + 7;
+    //     vector<vector<long long>> dp(n , vector<long long>(amt + 1 , 0));
+
+    //     for(long long i=0 ; i<=amt ; i++){
+    //         if(i % arr[0] == 0) dp[0][i] = 1;
+    //     }
+
+    //     for(long long ind=1 ; ind<n ; ind++){
+    //         for(long long w=0 ; w<=amt ; w++){
+    //             long long notTake = dp[ind - 1][w];
+    //             long long take = 0;
+    //             if(arr[ind] <= w) take = dp[ind][w - arr[ind]];
+
+    //             dp[ind][w] = (take + notTake) % mod;
+    //         }
+    //     }
+
+    //     return dp[n - 1][amt];
+    // }
     int change(int amt, vector<int>& arr) {
         int n = arr.size();
-        // vector<vector<int>> dp(n , vector<int>(amt + 1 , -1));
-        // return solve(n - 1 , amt , arr , dp);
-
         long long mod = 1e18 + 7;
-        vector<vector<long long>> dp(n , vector<long long>(amt + 1 , 0));
+
+        vector<long long> prev(amt + 1 , 0) , cur(amt + 1 , 0);
 
         for(long long i=0 ; i<=amt ; i++){
-            if(i % arr[0] == 0) dp[0][i] = 1;
+            if(i % arr[0] == 0) prev[i] = 1;
         }
 
         for(long long ind=1 ; ind<n ; ind++){
             for(long long w=0 ; w<=amt ; w++){
-                long long notTake = dp[ind - 1][w];
+                long long notTake = prev[w];
                 long long take = 0;
-                if(arr[ind] <= w) take = dp[ind][w - arr[ind]];
+                if(arr[ind] <= w) take = cur[w - arr[ind]];
 
-                dp[ind][w] = (take + notTake) % mod;
+                cur[w] = (take + notTake) % mod;
             }
+            prev = cur;
         }
 
-        return dp[n - 1][amt];
+        return prev[amt];
     }
 };
