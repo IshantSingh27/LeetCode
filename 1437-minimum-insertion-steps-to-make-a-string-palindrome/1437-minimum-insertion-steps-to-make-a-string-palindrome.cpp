d4be65e696704da194717046c7faa00996215e55
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-08-11 14:22:56 +0530

    Time: 17 ms (92.66%), Space: 9.8 MB (85.61%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    int solve(string s1 , string s2){
+int longestCommonSubsequence(string s1, string s2) {
         int n = s1.size() , m = s2.size();
+
         vector<int> prev(m + 1 , 0) , cur(m + 1 , 0);
+
         for(int i=1 ; i<=n ; i++){
-            for(int j=1  ; j<=m ; j++){
+            for(int j=1 ; j<=m ; j++){
                 if(s1[i - 1] == s2[j - 1]) cur[j] = 1 + prev[j - 1];
-                else cur[j] = max(cur[j - 1] , prev[j]);
+
+                else cur[j] = max(prev[j] , cur[j - 1]);
             }
             prev = cur;
         }
+
         return prev[m];
     }
     int longestPalindromeSubseq(string s1) {
         string s2 = s1;
         reverse(s2.begin() , s2.end());
-        return solve(s1 , s2);
+        return longestCommonSubsequence(s1 , s2);
     }
     int minInsertions(string s) {
         return s.size() - longestPalindromeSubseq(s);
