6a97e278209f3eee6f00d79a92ed97270ddd45d0
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-08-12 16:15:58 +0530

    Time: 23 ms (88.86%), Space: 26.8 MB (71.38%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -4,17 +4,16 @@ public:
         int n = s1.size() , m = s2.size();
         vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , 0));
 
-        for(int ind1 = 1 ; ind1 <= n ; ind1++){
-            for(int ind2 = 1 ; ind2 <= m ; ind2++){
-                if(s1[ind1 - 1] == s2[ind2 - 1]) dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=m ; j++){
+                if(s1[i - 1] == s2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
 
-                else dp[ind1][ind2] = 0 + max(dp[ind1 - 1][ind2] , dp[ind1][ind2 - 1]);
+                else dp[i][j] = max(dp[i-1][j] , dp[i][j - 1]);
             }
         }
 
         int i = n , j = m;
         string ans = "";
-
         while(i > 0 && j > 0){
             if(s1[i - 1] == s2[j - 1]){
                 ans += s1[i - 1];
