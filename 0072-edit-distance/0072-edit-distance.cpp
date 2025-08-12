a976b32bd04b9f19fcd32efb27dd6fbfbe8041a1
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-08-13 01:34:55 +0530

    Time: 3 ms (90.15%), Space: 10.4 MB (94.75%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -14,30 +14,55 @@ public:
 
         return dp[i][j] = 1 + min(del , min(rep , ins));
     }
+    // int minDistance(string s1, string s2) {
+    //     int n = s1.size() , m = s2.size();
+    //     // vector<vector<int>> dp(n , vector<int>(m , -1));
+    //     // return solve(n - 1 , m - 1 , s1 , s2 , dp);
+
+    //     vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , 0));
+
+    //     for(int i=0 ; i<= n ; i++) dp[i][0] = i;
+    //     for(int i=0 ; i<= m ; i++) dp[0][i] = i; //ariving in reverse only going bottom up
+
+    //     for(int i=1 ; i<=n ; i++){
+    //         for(int j=1 ; j<=m ; j++){
+    //             if(s1[i - 1] == s2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+
+    //             else{
+    //                 int del = dp[i - 1][j];
+    //                 int ins = dp[i][j - 1];
+    //                 int rep = dp[i - 1][j - 1];
+
+    //                 dp[i][j] = 1 + min(del , min(rep , ins));
+    //             }
+    //         }
+    //     }
+
+    //     return dp[n][m];
+    // }
     int minDistance(string s1, string s2) {
         int n = s1.size() , m = s2.size();
-        // vector<vector<int>> dp(n , vector<int>(m , -1));
-        // return solve(n - 1 , m - 1 , s1 , s2 , dp);
+        
+        vector<int> prev(m + 1 , 0) , cur(m + 1 , 0);
 
-        vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , 0));
-
-        for(int i=0 ; i<= n ; i++) dp[i][0] = i;
-        for(int i=0 ; i<= m ; i++) dp[0][i] = i;
+        for(int i=0 ; i<= m ; i++) prev[i] = i; //ariving in reverse only going bottom up
 
         for(int i=1 ; i<=n ; i++){
+            cur[0] = i;
             for(int j=1 ; j<=m ; j++){
-                if(s1[i - 1] == s2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+                if(s1[i - 1] == s2[j - 1]) cur[j] = prev[j - 1];
 
                 else{
-                    int del = dp[i - 1][j];
-                    int ins = dp[i][j - 1];
-                    int rep = dp[i - 1][j - 1];
+                    int del = prev[j];
+                    int ins = cur[j - 1];
+                    int rep = prev[j - 1];
 
-                    dp[i][j] = 1 + min(del , min(rep , ins));
+                    cur[j] = 1 + min(del , min(rep , ins));
                 }
             }
+            prev = cur;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
