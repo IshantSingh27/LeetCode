babac6c0a68785420cddc7c483cb96d848def698
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-09-27 02:05:15 +0530

    Time: 5 ms (58.52%), Space: 20.2 MB (22.71%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -5,35 +5,34 @@ public:
         for(auto it : edges){
             adj[it[0]].push_back(it[1]);
         }
-        
+
         vector<int> indeg(n , 0);
         for(int i=0 ; i<n ; i++){
             for(auto it : adj[i]){
                 indeg[it]++;
             }
         }
-        
+
         queue<int> q;
         for(int i=0 ; i<n ; i++){
-            if(indeg[i] == 0){
-                q.push(i);
-            }
+            if(indeg[i] == 0) q.push(i);
         }
-        
+
         vector<int> ans;
         while(!q.empty()){
             int node = q.front();
             ans.push_back(node);
             q.pop();
-            
+
             for(auto it : adj[node]){
                 indeg[it]--;
+
                 if(indeg[it] == 0){
                     q.push(it);
                 }
             }
         }
-        
+
         if(ans.size() == n) return true;
         else return false;
     }
