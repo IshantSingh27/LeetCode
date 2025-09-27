dee1bda55bed65fdb51182a42efc10ec060ed019
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-09-27 22:02:07 +0530

    Time: 0 ms (100%), Space: 18.5 MB (78.54%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -7,30 +7,32 @@ public:
             for(int j=0 ; j<m ; j++){
                 if(arr[i][j] == 0){
                     arr[i][0] = 0;
-                    
+
                     if(j != 0){
                         arr[0][j] = 0;
                     }
-                    else{
-                        col0 = 0;
-                    }
+                    else col0 = 0;
                 }
             }
         }
 
         for(int i=1 ; i<n ; i++){
             for(int j=1 ; j<m ; j++){
-                if(arr[i][0] == 0 || arr[0][j] == 0){
+                if(arr[0][j] == 0 || arr[i][0] == 0){
                     arr[i][j] = 0;
                 }
             }
         }
 
         if(arr[0][0] == 0){
-            for(int i=0 ; i<m ; i++) arr[0][i] = 0;
+            for(int j=0 ; j<m ; j++){
+                arr[0][j] = 0;
+            }
         }
         if(col0 == 0){
-            for(int i=0 ; i<n ; i++) arr[i][0] = 0;
+            for(int i=0 ; i<n ; i++){
+                arr[i][0] = 0;
+            }
         }
     }
 };
