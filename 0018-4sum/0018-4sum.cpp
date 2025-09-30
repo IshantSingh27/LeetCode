62e1e0064dc9b8821464a4be9fe775c90033179e
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-09-30 16:41:41 +0530

    Time: 23 ms (53.32%), Space: 17.4 MB (55.44%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,49 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<vector<int>> arr;
+    vector<vector<int>> fourSum(vector<int>& arr, int tar) {
+        long long n = arr.size();
+        vector<vector<int>> ans;
+        sort(arr.begin() , arr.end());
 
-        // If there are fewer than 4 numbers, return an empty result
-        if (n < 4) return {};
+        for(long long i=0 ; i<n ; i++){
+            if(i > 0 && arr[i] == arr[i - 1]) continue;
 
-        // Sort the array
-        sort(nums.begin(), nums.end());
+            for(long long j = i + 1 ; j<n ; j++){
+                if(j > i + 1 && arr[j] == arr[j - 1]) continue;
 
-        for (int i = 0; i < n - 3; i++) {
-            // Skip duplicate values for `i`
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+                long long k = j + 1 , l = n - 1;
+                while(k < l){
+                    long long sum = arr[i];
+                    sum += arr[j]; sum += arr[k]; sum += arr[l];
 
-            for (int j = i + 1; j < n - 2; j++) {
-                // Skip duplicate values for `j`
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                int k = j + 1;
-                int l = n - 1;
-
-                while (k < l) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
-
-                    if (sum == target) {
-                        arr.push_back({nums[i], nums[j], nums[k], nums[l]});
-
-                        // Skip duplicate values for `k`
-                        while (k + 1 < l && nums[k] == nums[k + 1]) k++;
-                        // Skip duplicate values for `l`
-                        while (l - 1 > k && nums[l] == nums[l - 1]) l--;
-
-                        // Move pointers after handling duplicates
+                    if(sum == tar){
+                        ans.push_back({arr[i] , arr[j] , arr[k] , arr[l]});
                         k++;
                         l--;
-                    } else if (sum < target) {
-                        k++; // Increase the sum by moving `k` forward
-                    } else {
-                        l--; // Decrease the sum by moving `l` backward
+                        while(k < l && arr[k] == arr[k - 1]) k++;
+                        while(l > k && arr[l] == arr[l + 1]) l--;
                     }
+                    else if(sum > tar) l--;
+                    else k++;
                 }
             }
         }
 
-        return arr;
+        return ans;
     }
 };
