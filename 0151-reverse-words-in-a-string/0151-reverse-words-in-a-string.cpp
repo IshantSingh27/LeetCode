39a65437d0634722c4c95b41e3e0ea8c60a03a37
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-10-01 22:15:09 +0530

    Time: 0 ms (100%), Space: 10.1 MB (78.19%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<char> temp;
-        string ans;
+        string ans = "";
 
         for(int i=0 ; i<s.size() ; i++){
-            if(s[i] == ' '){
-                if(i == 0) continue;
-                else if(s[i - 1] == ' ') continue;
-                else{
-                    for(int j=temp.size() - 1 ; j>=0 ; j--){
-                        ans += temp[j];
-                    }
-                    ans += ' ';
-                }
-                temp = {};
-            }
-
-            else{
-                temp.push_back(s[i]);
+            if(s[i] != ' ' || (!ans.empty() && ans.back() != ' ')){
+                ans += s[i];
             }
         }
+        if(ans.back() == ' ') ans.pop_back();
 
-        if(!temp.empty()){
-            for(int j=temp.size() - 1 ; j>=0 ; j--){
-                ans += temp[j];
-            }
-        }
-
-        if(ans[ans.size() - 1] == ' ') ans.pop_back();
         reverse(ans.begin() , ans.end());
+
+        int start = 0;
+        for(int i=0 ; i<=ans.size() ; i++){
+            if(i == ans.size() || ans[i] == ' '){
+                reverse(ans.begin() + start , ans.begin() + i);
+                start = i + 1;
+            }
+        }
+
         return ans;
     }
 };
