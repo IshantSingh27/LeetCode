322dea19b9ffa94e1b30eae45b49ee2ed3cbf0de
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-10-01 18:57:07 +0530

    Time: 0 ms (100%), Space: 9.3 MB (13.57%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        vector<vector<char>> arr;
         stack<char> st;
-        
+        string ans = "";
         vector<char> temp;
+
         for(int i=0 ; i<s.size() ; i++){
             temp.push_back(s[i]);
             if(s[i] == '(') st.push(s[i]);
             else st.pop();
 
             if(st.empty()){
-                arr.push_back(temp);
+                for(int i=1 ; i<=temp.size() - 2 ; i++){
+                    ans += temp[i];
+                }
                 temp = {};
-            }
-        }
-
-        string ans = "";
-        for(auto it : arr){
-            for(int i=1 ; i<=it.size() - 2 ; i++){
-                ans += it[i];
             }
         }
 
