a2dd01de1d5cbfde4853e4b81174eac246f07b77
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-10-17 12:51:18 +0530

    Time: 0 ms (100%), Space: 46.3 MB (47.16%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int maxScore(vector<int>& arr, int k) {
-        int n = arr.size() , lsum = 0 , rsum = 0;
-        for(int i=0 ; i<k ; i++) lsum += arr[i];
-        if(k == n) return lsum;
-        int maxi = lsum , r = n - 1;
-        // cout<<"maxi : "<<maxi<<endl;
-        for(int i = k-1 ; i >= 0 ; i--){
-            // cout<<"arr[i] : "<<arr[i]<<" arr[n - i - 1] : "<<arr[n - i - 1];
+        int n = arr.size() , lsum = 0 , rsum = 0 , maxi = 0 , rind = n - 1;
+
+        for(int i=0 ; i<k ; i++){
+            lsum += arr[i];
+            maxi = max(maxi , lsum);
+        }
+
+        for(int i=k - 1 ; i>=0 ; i--){
             lsum -= arr[i];
-            rsum += arr[r--];
-            // cout<<" total : "<<lsum + rsum<<endl;
+
+            rsum += arr[rind];
+            rind--;
+
             maxi = max(maxi , lsum + rsum);
         }
+
         return maxi;
     }
 };
