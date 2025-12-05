994bddab0156e2432f8b8cf8e58e2918658615fe
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-12-05 13:09:55 +0530

    Time: 0 ms (100%), Space: 18.1 MB (55.53%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    void solve(int row, int col, vector<vector<int>>& arr, vector<vector<int>>& ans, int color, int newcol){
-        ans[row][col] = newcol;
-        vector<int> drow = {-1 , 0 , 1 , 0};
-        vector<int> dcol = {0 , 1 , 0 , -1};
+    void dfs(int row , int col , vector<vector<int>>& arr , vector<vector<int>>& vis , int color , int old){
         int n = arr.size() , m = arr[0].size();
+        vis[row][col] = color;
+
+        vector<int> drow = {-1, 0 , 1 , 0};
+        vector<int> dcol = {0, 1 , 0 , -1};
 
         for(int i=0 ; i<4 ; i++){
             int nrow = row + drow[i];
             int ncol = col + dcol[i];
-
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && arr[nrow][ncol] == color && ans[nrow][ncol] != newcol){
-                solve(nrow , ncol , arr , ans , color , newcol);
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && arr[nrow][ncol] == old && vis[nrow][ncol] != color){
+                dfs(nrow , ncol , arr , vis , color , old);
             }
         }
     }
 
-    vector<vector<int>> floodFill(vector<vector<int>>& arr, int sr, int sc, int newcol) {
-        int n = arr.size() , m = arr[0].size();
-        vector<vector<int>> ans = arr;
-        int color = arr[sr][sc];
+    vector<vector<int>> floodFill(vector<vector<int>>& arr, int sr, int sc, int color) {
+        int n = arr.size() , m = arr[0].size() , old = arr[sr][sc];
+        vector<vector<int>> vis(n , vector<int>(m , 0));
 
-        solve(sr , sc , arr , ans , color , newcol);
+        dfs(sr , sc , arr , vis , color , old);
 
-        return ans;
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
+                if(vis[i][j] == 0){
+                    vis[i][j] = arr[i][j];
+                }
+            }
+        }
+
+        return vis;
     }
 };
