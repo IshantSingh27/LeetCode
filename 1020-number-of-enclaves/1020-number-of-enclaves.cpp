18b30c9a1c93b57f58a3c7770a1e51df8712fce9
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-12-06 21:34:08 +0530

    Time: 205 ms (7.2%), Space: 58.8 MB (5.05%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void dfs(int row , int col , vector<vector<int>>& vis , vector<vector<int>>& arr){
+        int n = arr.size() , m = arr[0].size();
+        vis[row][col] = 1;
+
+        vector<int> drow = {-1 , 0 , 1 , 0};
+        vector<int> dcol = {0 , 1 , 0 , -1};
+
+        for(int i=0 ; i<4 ; i++){
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && arr[nrow][ncol] == 1){
+                dfs(nrow , ncol , vis , arr);
+            }
+        }
+    }
+
+    int numEnclaves(vector<vector<int>>& arr) {
+        int n = arr.size() , m = arr[0].size() , cnt = 0;
+        vector<vector<int>> vis(n , vector<int>(m , 0));
+
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
+                if(i == 0 || j == 0 || i == n-1 || j == m-1){
+                    if(!vis[i][j] && arr[i][j] == 1){
+                        dfs(i , j , vis , arr);
+                    }
+                }
+            }
+        }
+
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
+                if(arr[i][j] == 1 && !vis[i][j]){
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
