21803d7a171c8edb61875d0ba665a7f6e7e1787d
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-12-06 19:48:04 +0530

    Time: 3 ms (25.13%), Space: 16 MB (13.3%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void dfs(int row , int col , vector<vector<char>>& arr , vector<vector<int>>& vis){
+    void dfs(int row , int col , vector<vector<int>>& vis , vector<vector<char>>& arr){
         int n = arr.size() , m = arr[0].size();
         vis[row][col] = 1;
 
@@ -12,39 +12,28 @@ public:
             int ncol = col + dcol[i];
 
             if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && arr[nrow][ncol] == 'O'){
-                dfs(nrow , ncol , arr , vis);
+                dfs(nrow , ncol , vis , arr);
             }
         }
     }
-
+ 
     void solve(vector<vector<char>>& arr) {
         int n = arr.size() , m = arr[0].size();
         vector<vector<int>> vis(n , vector<int>(m , 0));
 
-        for(int i=0 ; i<m ; i++){
-            if(!vis[0][i] && arr[0][i] == 'O'){
-                dfs(0 , i , arr , vis);
-            }
-        }
-        for(int i=0 ; i<m ; i++){
-            if(!vis[n-1][i] && arr[n-1][i] == 'O'){
-                dfs(n-1 , i , arr , vis);
-            }
-        }
         for(int i=0 ; i<n ; i++){
-            if(!vis[i][0] && arr[i][0] == 'O'){
-                dfs(i , 0 , arr , vis);
-            }
-        }
-        for(int i=0 ; i<n ; i++){
-            if(!vis[i][m-1] && arr[i][m-1] == 'O'){
-                dfs(i , m-1 , arr , vis);
+            for(int j=0 ; j<m ; j++){
+                if(i == 0 || j == 0 || i == n-1 || j == m-1){
+                    if(!vis[i][j] && arr[i][j] == 'O'){
+                        dfs(i , j , vis , arr);
+                    }
+                }
             }
         }
 
         for(int i=0 ; i<n ; i++){
             for(int j=0 ; j<m ; j++){
-                if(!vis[i][j]){
+                if(arr[i][j] == 'O' && !vis[i][j]){
                     arr[i][j] = 'X';
                 }
             }
