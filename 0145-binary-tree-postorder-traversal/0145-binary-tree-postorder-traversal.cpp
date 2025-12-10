2259c54548dc101cc8a8c399bf9e5ec096fba7e5
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-12-10 23:30:59 +0530

    Time: 0 ms (100%), Space: 11 MB (43.6%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -19,25 +19,59 @@ public:
         ans.push_back(root->val);
     }
 
+    // vector<int> postorderTraversal(TreeNode* root) {
+    //     vector<int> ans;
+    //     if(root == NULL) return ans;
+
+    //     // post(root , ans);
+
+    //     stack<TreeNode*> st2;
+    //     st2.push(root);
+
+    //     while(!st2.empty()){
+    //         TreeNode* temp = st2.top();
+    //         st2.pop();
+    //         ans.push_back(temp->val);
+
+    //         if(temp->left) st2.push(temp->left);
+    //         if(temp->right) st2.push(temp->right);
+    //     }
+
+    //     reverse(ans.begin() , ans.end());
+
+    //     return ans;
+    // }
+
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
         if(root == NULL) return ans;
 
         // post(root , ans);
 
-        stack<TreeNode*> st2;
-        st2.push(root);
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
 
-        while(!st2.empty()){
-            TreeNode* temp = st2.top();
-            st2.pop();
-            ans.push_back(temp->val);
+        while(cur || !st.empty()){
+            if(cur){
+                st.push(cur);
+                cur = cur->left;
+            }
+            else{
+                TreeNode* temp = st.top()->right;
+                if(temp == NULL){
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
 
-            if(temp->left) st2.push(temp->left);
-            if(temp->right) st2.push(temp->right);
+                    while(!st.empty() && temp == st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else cur = temp;
+            }
         }
-
-        reverse(ans.begin() , ans.end());
 
         return ans;
     }
