3e955089b1dc5b0c0cb67ffe70953d5ce50c62af
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-12-27 12:44:15 +0530

    Time: 8 ms (37.09%), Space: 20.6 MB (16.23%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
+    bool canFinish(int v, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(v);
         for(auto it : edges){
             adj[it[0]].push_back(it[1]);
         }
-
-        vector<int> indeg(n , 0);
-        for(int i=0 ; i<n ; i++){
-            for(auto it : adj[i]){
-                indeg[it]++;
+        
+        vector<int> indeg(v , 0);
+        for(auto it : adj){
+            for(auto it1 : it){
+                indeg[it1]++;
             }
         }
-
+        
         queue<int> q;
-        for(int i=0 ; i<n ; i++){
+        for(int i=0 ; i<v ; i++){
             if(indeg[i] == 0) q.push(i);
         }
-
+        
         vector<int> ans;
         while(!q.empty()){
             int node = q.front();
-            ans.push_back(node);
             q.pop();
-
+            
+            ans.push_back(node);
+            
             for(auto it : adj[node]){
                 indeg[it]--;
-
-                if(indeg[it] == 0){
-                    q.push(it);
-                }
+                
+                if(indeg[it] == 0) q.push(it);
             }
         }
-
-        if(ans.size() == n) return true;
-        else return false;
+        
+        if(ans.size() != v) return false;
+        else return true;
     }
 };
