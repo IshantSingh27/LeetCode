8f740be4b3f966263f6fbd8838d67be8db5fb5d4
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2025-12-27 12:54:45 +0530

    Time: 15 ms (7.62%), Space: 19.3 MB (15.28%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
+    vector<int> findOrder(int v, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(v);
         for(auto it : edges){
-            adj[it[1]].push_back(it[0]);
+            adj[it[0]].push_back(it[1]);
         }
         
-        vector<int> indeg(n , 0);
-        for(int i=0 ; i<n ; i++){
-            for(auto it : adj[i]){
-                indeg[it]++;
+        vector<int> indeg(v , 0);
+        for(auto it : adj){
+            for(auto it1 : it){
+                indeg[it1]++;
             }
         }
         
         queue<int> q;
-        for(int i=0 ; i<n ; i++){
-            if(indeg[i] == 0){
-                q.push(i);
-            }
+        for(int i=0 ; i<v ; i++){
+            if(indeg[i] == 0) q.push(i);
         }
         
         vector<int> ans;
         while(!q.empty()){
             int node = q.front();
-            ans.push_back(node);
             q.pop();
+            
+            ans.push_back(node);
             
             for(auto it : adj[node]){
                 indeg[it]--;
-                if(indeg[it] == 0){
-                    q.push(it);
-                }
+                
+                if(indeg[it] == 0) q.push(it);
             }
         }
+
+        reverse(ans.begin() , ans.end());
         
-        if(ans.size() == n) return ans;
-        else return {};
+        if(ans.size() != v) return {};
+        else return ans;
     }
 };
