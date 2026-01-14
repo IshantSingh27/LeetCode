1e15ac0f4e974272515af3f584390fa3082b899b
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-01-14 23:56:41 +0530

    Time: 135 ms (47.51%), Space: 110.5 MB (31.85%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
-    int solve(int ind , int tar , vector<int>& arr , vector<vector<int>>& dp){
-        if(tar == 0) return true;
-        if(ind == 0) return arr[0] == tar;
-
-        if(dp[ind][tar] != -1) return dp[ind][tar];
-
-        bool notTake = solve(ind - 1 , tar , arr , dp);
-        bool take = 0;
-        if(arr[ind] <= tar){
-            take = solve(ind - 1 , tar - arr[ind] , arr , dp);
+    bool solve(int ind , int k , vector<vector<int>>& dp , vector<int>& arr){
+        if(ind == 0){
+            if(arr[0] == k) return true;
+            else return false;
         }
 
-        return dp[ind][tar] = take || notTake;
+        if(dp[ind][k] != -1) return dp[ind][k];
+
+        bool nottake = solve(ind - 1 , k , dp , arr);
+        bool take = false;
+        if(arr[ind] <= k) take = solve(ind - 1 , k - arr[ind] , dp , arr);
+
+        return dp[ind][k] = take || nottake; 
     }
+
     bool canPartition(vector<int>& arr) {
         int n = arr.size() , sum = 0;
         for(int i=0 ; i<n ; i++){
             sum += arr[i];
         }
-        if(sum % 2 != 0) return false;
-        // vector<vector<int>> dp(n , vector<int>( (sum / 2) + 1 , -1 ));
-        // return solve(n - 1 , sum / 2 , arr , dp);
+        if(sum % 2 == 1) return false;
         int k = sum / 2;
-        vector<int> prev(k + 1 , false) , cur(k + 1 , false);
-        prev[0] = false ; cur[0] = true;
-        if(arr[0] <= k) prev[arr[0]] = true;
-        for(int ind = 1 ; ind < n ; ind++){
-            for(int tar = 1 ; tar <= k ; tar++){
-                bool notTake = prev[tar];
-                bool take = 0;
-                if(arr[ind] <= tar){
-                    take = prev[tar - arr[ind]];
-                }
-        
-                cur[tar] = take || notTake;
-            }
-            prev = cur;
-        }
-        return prev[k];
+
+        vector<vector<int>> dp(n , vector<int>(k + 1 , -1));
+
+        return solve(n - 1 , k , dp ,  arr);
     }
 };
