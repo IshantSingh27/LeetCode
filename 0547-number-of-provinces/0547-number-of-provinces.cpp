7bff1843bd1cb68a116b857424e675f9bb2b1abf
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-01-16 08:13:06 +0530

    Time: 9 ms (7.86%), Space: 21.2 MB (11.78%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    
     void dfs(int node , vector<int>& vis , vector<vector<int>>& adj){
         vis[node] = 1;
 
         for(auto it : adj[node]){
             if(!vis[it]){
-                dfs(it  , vis , adj);
+                dfs(it , vis , adj);
             }
         }
     }
@@ -22,13 +21,13 @@ public:
                 }
             }
         }
-
         vector<int> vis(n , 0);
         int cnt = 0;
+
         for(int i=0 ; i<n ; i++){
             if(!vis[i]){
-                dfs(i , vis , adj);
                 cnt++;
+                dfs(i , vis , adj);
             }
         }
 
