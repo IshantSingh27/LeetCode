b59eaada2c4c43b66045fb35250f0b1488006bfd
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-03-10 13:32:31 +0530

    Time: 0 ms (100%), Space: 11.8 MB (62.99%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,7 +4,7 @@ public:
         string ans = "";
         for(int i=0 ; i<s[0].size() ; i++){
             for(int j=1 ; j<s.size() ; j++){
-                if(s[j][i] != s[j - 1][i]) return ans;
+                if(i >= s[j].size() || s[j][i] != s[j - 1][i]) return ans;
             }
             ans += s[0][i];
         }
