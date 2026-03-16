258b4a3f3ea3160f471c7c57bc11359eb23af65b
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-03-16 17:18:45 +0530

    Time: 99 ms (69.78%), Space: 110.3 MB (45.08%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,47 +1,52 @@
 class Solution {
 public:
-    bool solve(int ind , int k , vector<vector<int>>& dp , vector<int>& arr){
-        if(ind == 0){
-            if(arr[0] == k) return true;
-            else return false;
-        }
+    bool solve(int ind , int k , vector<int> arr , vector<vector<int>>& dp){
+        if(k == 0) return true;
+        else if(ind == 0) return arr[0] == k;
 
         if(dp[ind][k] != -1) return dp[ind][k];
 
-        bool nottake = solve(ind - 1 , k , dp , arr);
-        bool take = false;
-        if(arr[ind] <= k) take = solve(ind - 1 , k - arr[ind] , dp , arr);
+        int nottake = solve(ind - 1 , k , arr , dp);
+        int take = false;
+        if(arr[ind] <= k){
+            take = solve(ind - 1 , k - arr[ind] , arr , dp);
+        }
 
-        return dp[ind][k] = take || nottake; 
+        return dp[ind][k] = take || nottake;
     }
 
     bool canPartition(vector<int>& arr) {
-        int n = arr.size() , sum = 0;
+        int n = arr.size() , sum = 0 , tar = 0;
         for(int i=0 ; i<n ; i++){
             sum += arr[i];
         }
-        if(sum % 2 == 1) return false;
-        int k = sum / 2;
 
-        // vector<vector<int>> dp(n , vector<int>(k + 1 , -1));
-
-        // return solve(n - 1 , k , dp ,  arr);
-
-        vector<vector<bool>> dp(n + 1 , vector<bool>(k + 1 , false));
-        for(int i=0 ; i<=n ; i++){
-            dp[i][0] = true;
+        if(sum % 2 != 0) return false;
+        else{
+            tar = sum / 2;
         }
 
-        for(int i=1 ; i<=n ; i++){
-            for(int j=1 ; j<=k ; j++){
-                bool nottake = dp[i - 1][j];
-                bool take = false;
-                if(arr[i - 1] <= j) take = dp[i - 1][j - arr[i - 1]];
+        // vector<vector<int>> dp(n , vector<int>(tar + 1 , -1));
 
-                dp[i][j] = take || nottake;
+        // return solve(n - 1 , tar , arr , dp);
+
+        vector<vector<int>> dp(n , vector<int>(tar + 1 , 0));
+
+        for(int i=0 ; i<n ; i++){
+            dp[i][0] = true;
+        }
+        if(arr[0] <= tar) dp[0][arr[0]] = true;
+
+        for(int ind=1 ; ind<n ; ind++){
+            for(int k=1 ; k<=tar ; k++){
+                int nottake = dp[ind - 1][k];
+                int take = false;
+                if(arr[ind] <= k) take = dp[ind - 1][k - arr[ind]];
+
+                dp[ind][k] = take || nottake;
             }
         }
 
-        return dp[n][k];
+        return dp[n - 1][tar];
     }
 };
