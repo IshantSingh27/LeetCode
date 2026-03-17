f638576690805c57cdece667bc7cb5f53f7d3b44
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-03-17 17:56:02 +0530

    Time: 6 ms (70.63%), Space: 15.5 MB (48.74%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& arr, int d) {
-        int total = 0 , n = arr.size();
+    int solve(int ind , int k , vector<int>& arr , vector<vector<int>>& dp){
+        if(ind == 0){
+            if(k == 0 && arr[ind] == 0) return 2;
+            if(k ==0 || arr[ind] == k) return 1;
+            return 0;
+        }
+
+        if(dp[ind][k] != -1) return dp[ind][k];
+
+        int nottake = solve(ind - 1 , k , arr , dp);
+        int take = 0;
+        if(arr[ind] <= k) take = solve(ind - 1 , k - arr[ind] , arr , dp);
+
+        return dp[ind][k] = take + nottake;
+    }
+
+    int findTargetSumWays(vector<int>& arr, int tar) {
+        int n = arr.size() , sum = 0;
         for(int i=0 ; i<n ; i++){
-            total += arr[i];
+            sum += arr[i];
         }
-        int sum = (total + d) / 2;
-        if(total < abs(d) || (total + d) % 2 != 0) return 0;
-        vector<int> prev(sum + 1 , 0) , cur(sum + 1 , 0);
-        if(arr[0] == 0) prev[0] = 2;
-        else prev[0] = 1;
-        if(arr[0] != 0 && arr[0] <= sum) prev[arr[0]] = 1;
-        for(int ind = 1 ; ind < n ; ind++){
-            for(int tar = 0 ; tar <= sum ; tar++){
-                int notTake = prev[tar];
-                int take = 0;
-                if(arr[ind] <= tar){
-                    take = prev[tar - arr[ind]];
-                }
-                cur[tar] = take + notTake;
-            }
-            prev = cur;
-        }
-        return prev[sum];
+        int k = sum - tar;
+        if(sum - tar < 0 || (sum - tar) % 2 != 0) return 0;
+        else k = k / 2;
+
+        vector<vector<int>> dp(n , vector<int>(k + 1 , -1));
+
+        return solve(n - 1 , k , arr , dp);
     }
 };
