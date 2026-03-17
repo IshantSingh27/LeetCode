2b0d791746079c4ab55bd996c2dd031efe50cb2d
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-03-17 18:10:11 +0530

    Time: 4 ms (77.59%), Space: 15.5 MB (48.74%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -3,7 +3,7 @@ public:
     int solve(int ind , int k , vector<int>& arr , vector<vector<int>>& dp){
         if(ind == 0){
             if(k == 0 && arr[ind] == 0) return 2;
-            if(k ==0 || arr[ind] == k) return 1;
+            if(k == 0 || arr[ind] == k) return 1;
             return 0;
         }
 
@@ -25,8 +25,28 @@ public:
         if(sum - tar < 0 || (sum - tar) % 2 != 0) return 0;
         else k = k / 2;
 
-        vector<vector<int>> dp(n , vector<int>(k + 1 , -1));
+        // vector<vector<int>> dp(n , vector<int>(k + 1 , -1));
 
-        return solve(n - 1 , k , arr , dp);
+        // return solve(n - 1 , k , arr , dp);
+
+        vector<vector<int>> dp(n , vector<int>(k + 1 , 0));
+
+        for(int i=0 ; i<n ; i++){
+            dp[i][0] = 1;
+        }
+        if(arr[0] == 0) dp[0][arr[0]] = 2;
+        else if(arr[0] <= k) dp[0][arr[0]] = 1;
+
+        for(int ind=1 ; ind<n ; ind++){
+            for(int j=0 ; j<= k ; j++){
+                int nottake = dp[ind - 1][j];
+                int take = 0;
+                if(arr[ind] <= j) take = dp[ind - 1][j - arr[ind]];
+
+                dp[ind][j] = take + nottake;
+            }
+        }
+
+        return dp[n - 1][k];
     }
 };
