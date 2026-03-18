72e9489b84bbfa6a370f85c0df9571b348d6afb7
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-03-18 13:03:54 +0530

    Time: 4 ms (44.49%), Space: 23.4 MB (66.92%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,7 +1,53 @@
+// class MinStack {
+// public:
+// stack<pair<int , int>> st;
+// int mini = INT_MAX;
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         if(st.empty()){
+//             mini = val;
+//             st.push({val , mini});
+//         }
+//         else{
+//             mini = min(mini , val);
+//             st.push({val , mini});
+//         }
+//     }
+    
+//     void pop() {
+//         if(st.empty()) return;
+//         st.pop();
+//         if(st.empty()){
+//             mini = INT_MAX;
+//             return;
+//         }
+//         mini = st.top().second;
+//     }
+    
+//     int top() {
+//         return st.top().first;
+//     }
+    
+//     int getMin() {
+//         return st.top().second;
+//     }
+// };
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack* obj = new MinStack();
+//  * obj->push(val);
+//  * obj->pop();
+//  * int param_3 = obj->top();
+//  * int param_4 = obj->getMin();
+//  */
 class MinStack {
 public:
-stack<pair<int , int>> st;
-int mini = INT_MAX;
+stack<long long> st;
+long long mini = INT_MAX;
     MinStack() {
         
     }
@@ -9,30 +55,35 @@ int mini = INT_MAX;
     void push(int val) {
         if(st.empty()){
             mini = val;
-            st.push({val , mini});
+            st.push(val);
         }
         else{
-            mini = min(mini , val);
-            st.push({val , mini});
+            if(val > mini) st.push(val);
+            else{
+                st.push(2LL * val - mini);
+                mini = val;
+            }
         }
     }
     
     void pop() {
         if(st.empty()) return;
+        long long n = st.top();
         st.pop();
         if(st.empty()){
             mini = INT_MAX;
             return;
         }
-        mini = st.top().second;
+        if(n < mini) mini = (2LL * mini - n);
     }
     
     int top() {
-        return st.top().first;
+        if(st.top() < mini) return mini;
+        return st.top();
     }
     
     int getMin() {
-        return st.top().second;
+        return mini;
     }
 };
 
