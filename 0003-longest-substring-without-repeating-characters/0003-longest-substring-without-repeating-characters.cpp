edea3d6158f2aa6f559d58eba3693477f8fe2539
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-03-20 00:26:43 +0530

    Time: 7 ms (63.4%), Space: 11.8 MB (55.1%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        if(n == 0 || n == 1) return n;
         unordered_map<char , int> mp;
-        for(char i='a' ; i<='z' ; i++){
-            mp[i] = -1;
-        }
-
-        int l = 0 , r = 0 , maxlen = 1;
-        while(r < n){
-            if(mp[s[r]] != -1){
-                l = max(l , mp[s[r]] + 1);
+        int n = s.size() , i = 0 , j = 0 , ans = 0;
+        while(i < n){
+            if(mp[s[i]] == 0) mp[s[i]]++;
+            else{
+                while(j < i){
+                    mp[s[j]]--;
+                    j++;
+                    if(mp[s[i]] == 0) break;
+                }
+                mp[s[i]]++;
             }
-            maxlen = max(maxlen , r  - l + 1);
-            // cout<<"len : "<<r - l + 1<<" | ";
-
-            mp[s[r]] = r;
-            r++;
-            // cout<<"maxlen : "<<maxlen<<endl;
+            ans = max(ans , i - j + 1);
+            i++;
         }
 
-        return maxlen;
+        return ans;
     }
 };
