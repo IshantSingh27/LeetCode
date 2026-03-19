f1890c54c26feec8494c3fd140819b373c453806
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-03-19 09:20:32 +0530

    Time: 2 ms (80.14%), Space: 15.9 MB (40.44%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,102 +1,99 @@
 class Solution {
 public:
     vector<int> nextsmall(vector<int>& arr){
-        stack<int> st;
         int n = arr.size();
         vector<int> ans(n);
+        stack<int> st;
 
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
-
+        for(int i=n-1 ; i>=0 ; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+                st.pop();
+            }
             if(st.empty()) ans[i] = n;
             else ans[i] = st.top();
 
             st.push(i);
         }
+
         return ans;
     }
 
     vector<int> prevsmall(vector<int>& arr){
-        stack<int> st;
         int n = arr.size();
         vector<int> ans(n);
+        stack<int> st;
 
-        for(int i=0;i<n;i++){
-            while(!st.empty() && arr[st.top()] > arr[i]) st.pop();
-
+        for(int i=0 ; i<n ; i++){
+            while(!st.empty() && arr[st.top()] > arr[i]){
+                st.pop();
+            }
             if(st.empty()) ans[i] = -1;
             else ans[i] = st.top();
 
             st.push(i);
         }
+
         return ans;
     }
 
-    long long sumSubarrayMins(vector<int>& arr) {
-        vector<int> nse = nextsmall(arr);
-        vector<int> pse = prevsmall(arr);
-
-        long long tot = 0;
-        int n = arr.size();
-
-        for(int i=0;i<n;i++){
-            long long left = i - pse[i];
-            long long right = nse[i] - i;
-            tot += left * right * arr[i];
-        }
-
-        return tot;
-    }
-
     vector<int> nextgreat(vector<int>& arr){
-        stack<int> st;
         int n = arr.size();
         vector<int> ans(n);
+        stack<int> st;
 
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && arr[st.top()] <= arr[i]) st.pop();
-
+        for(int i=n-1 ; i>=0 ; i--){
+            while(!st.empty() && arr[st.top()] <= arr[i]){
+                st.pop();
+            }
             if(st.empty()) ans[i] = n;
             else ans[i] = st.top();
 
             st.push(i);
         }
+
         return ans;
     }
 
     vector<int> prevgreat(vector<int>& arr){
-        stack<int> st;
         int n = arr.size();
         vector<int> ans(n);
+        stack<int> st;
 
-        for(int i=0;i<n;i++){
-            while(!st.empty() && arr[st.top()] < arr[i]) st.pop();
-
+        for(int i=0 ; i<n ; i++){
+            while(!st.empty() && arr[st.top()] < arr[i]){
+                st.pop();
+            }
             if(st.empty()) ans[i] = -1;
             else ans[i] = st.top();
 
             st.push(i);
         }
+
         return ans;
     }
 
-    long long sumSubarrayMaxs(vector<int>& arr) {
-        vector<int> nge = nextgreat(arr);
-        vector<int> pge = prevgreat(arr);
+    long long subArrayRanges(vector<int>& arr) {
+        vector<int> nse = nextsmall(arr);
+        vector<int> pse = prevsmall(arr);
+        long long n = arr.size() , tot = 0 , mod = 1e18 + 7;
 
-        long long tot = 0;
-        int n = arr.size();
-
-        for(int i=0;i<n;i++){
-            long long left = i - pge[i];
-            long long right = nge[i] - i;
-            tot += left * right * arr[i];
+        for(int i=0 ; i<n ; i++){
+            long long right = (nse[i] - i);
+            long long left = (i - pse[i]);
+            tot = (tot + ((right * left) % mod * arr[i] * 1LL) % mod) % mod;
         }
 
-        return tot;
-    }
+        vector<int> nge = nextgreat(arr);
+        vector<int> pge = prevgreat(arr);
+        long long tot1 = 0;
 
-    long long subArrayRanges(vector<int>& nums) {
-        return sumSubarrayMaxs(nums) - sumSubarrayMins(nums);
+        for(int i=0 ; i<n ; i++){
+            long long right = (nge[i] - i);
+            long long left = (i - pge[i]);
+            tot1 = (tot1 + ((right * left) % mod * arr[i] * 1LL) % mod) % mod;
+        }
+
+        long long ans = tot1 - tot;
+        return ans;
     }
 };
