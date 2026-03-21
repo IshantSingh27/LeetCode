3372d6a021eae82226290a8e385331932f9ed231
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-03-21 16:07:07 +0530

    Time: 9 ms (51.89%), Space: 11.3 MB (81.18%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size() , m = t.size() , minlen = 1e9 , l = 0 , r = 0 , cnt = 0 , sind = -1;
+        int n = s.size() , m = t.size() , l = 0 , r = 0 , minlen = 1e9 , cnt = 0 , sind = -1;
+
         unordered_map<char , int> mp;
         for(int i=0 ; i<m ; i++) mp[t[i]]++;
+
         while(r < n){
             if(mp[s[r]] > 0) cnt++;
             mp[s[r]]--;
+
             while(cnt == m){
-                if(r - l + 1 < minlen){
-                    minlen = r - l + 1;
+                mp[s[l]]++;
+
+                if(minlen > r - l  + 1){
+                    minlen = r - l  + 1;
                     sind = l;
                 }
-                mp[s[l]]++;
+
                 if(mp[s[l]] > 0) cnt--;
                 l++;
             }
+
             r++;
         }
+
         if(sind == -1) return "";
         else return s.substr(sind , minlen);
     }
