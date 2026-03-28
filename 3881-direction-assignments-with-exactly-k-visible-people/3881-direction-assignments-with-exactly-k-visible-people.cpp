790ac8e5050a98cb8a5ac21ea2e7c97a2a41fde7
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-03-29 00:21:37 +0530

    Time: 47 ms (56.72%), Space: 74.6 MB (36.22%) - LeetHub

---
+++ 3881-direction-assignments-with-exactly-k-visible-people/3881-direction-assignments-with-exactly-k-visible-people.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    using ll = long long;
+    const ll MOD = 1e9 + 7;
+
+    vector<ll> fact, invfact;
+
+    ll power(ll a, ll b){
+        ll res = 1;
+        while(b){
+            if(b & 1) res = (res * a) % MOD;
+            a = (a * a) % MOD;
+            b >>= 1;
+        }
+        return res;
+    }
+
+    void init(int n){
+        fact.resize(n+1);
+        invfact.resize(n+1);
+
+        fact[0] = 1;
+        for(int i=1;i<=n;i++) fact[i] = fact[i-1]*i % MOD;
+
+        invfact[n] = power(fact[n], MOD-2);
+        for(int i=n-1;i>=0;i--) invfact[i] = invfact[i+1]*(i+1)%MOD;
+    }
+
+    ll nCr(int n, int r){
+        if(r < 0 || r > n) return 0;
+        return fact[n] * invfact[r] % MOD * invfact[n-r] % MOD;
+    }
+    
+    int countVisiblePeople(int n, int pos, int k) {
+        init(n);
+        return (2LL * nCr(n-1, k)) % MOD;
+    }
+};
