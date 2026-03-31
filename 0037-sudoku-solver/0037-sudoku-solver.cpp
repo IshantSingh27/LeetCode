d093cde677376c65021f5ea91f4c281f71f9d2b5
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-03-31 13:13:47 +0530

    Time: 486 ms (13.23%), Space: 8.7 MB (93.33%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-bool safe(int row , int col , vector<vector<char>> &arr , char k){
+    bool chk(int row , int col , char c , vector<vector<char>>& arr){
         for(int i=0 ; i<9 ; i++){
-            if(arr[row][i] == k) return false;
-            
-            if(arr[i][col] == k) return false;
-            
-            if(arr[(3 * (row / 3)) + (i / 3)][(3 * (col / 3)) + (i % 3)] == k) return false;
+            if(i != col && arr[row][i] == c) return false;
+
+            if(i != row && arr[i][col] == c) return false;
+
+            if(arr[(3 * (row / 3)) + (i / 3)][(3 * (col / 3)) + (i % 3)] == c) return false;
         }
-        
+
         return true;
     }
-  
-    bool solve(vector<vector<char>> &arr){
+
+    bool solve(vector<vector<char>>& arr){
         for(int i=0 ; i<9 ; i++){
             for(int j=0 ; j<9 ; j++){
-                if(arr[i][j] == '.'){
-                    
-                    for(char k='1' ; k<='9' ; k++){
-                        if(safe(i , j , arr , k)){
-                            arr[i][j] = k;
-                            
-                            if(solve(arr)) return true;
-                            else{
-                                arr[i][j] = '.';
-                            }
-                        }
+                if(arr[i][j] != '.') continue;
+
+                for(char k='1' ; k<='9' ; k++){
+                    if(chk(i , j , k , arr)){
+                        arr[i][j] = k;
+                        if(solve(arr)) return true;
+                        arr[i][j] = '.';
                     }
-                    
-                    return false;
                 }
+
+                return false;
             }
         }
-        
+
         return true;
     }
 
