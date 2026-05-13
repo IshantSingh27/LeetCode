f40ba8565372f29f5d2d424c718df6e99034e54a
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-13 21:37:19 +0530

    Time: 0 ms (100%), Space: 52.3 MB (55.53%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
     bool canJump(vector<int>& arr) {
-        int maxi = 0 , n = arr.size();
-        for(int i=0 ; i<n ; i++){
-            if(i > maxi) return false;
-            if(maxi >= n - 1) return true;
-            maxi = max(maxi , i + arr[i]);
+        int n = arr.size() , i = 0;
+        while(i < n){
+            int jump = 0 , j = arr[i] , ind = 0;
+
+            // cout<<"j : "<<j<<endl;
+
+            if(i + j >= n - 1) return true;
+
+            for(int k=1 ; k<=j ; k++){
+                // cout<<"arr[i + k] : "<<arr[i + k]<<endl;
+                if(arr[i + k] + i + k >= jump){
+                    jump = arr[i + k] + i + k;
+                    ind = i + k;
+                    // cout<<"k : "<<k<<" jump : "<<jump<<endl;
+                }
+            }
+
+            // cout<<"JUMP : "<<jump<<endl;
+
+            if(ind == 0) return false;
+
+            i = ind;
         }
-        return true;
+
+        return false;
     }
 };
