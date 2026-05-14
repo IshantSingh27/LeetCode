9d47373147567301ed4c30000283a66acbd1598a
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-14 21:13:41 +0530

    Time: 0 ms (100%), Space: 22.8 MB (77.91%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,33 +1,3 @@
-// class Solution {
-// public:
-//     int candy(vector<int>& arr) {
-//         int i = 1 , sum = 1 , n = arr.size();
-
-//         while(i < n){
-//             while(i < n && arr[i] == arr[i - 1]){
-//                 sum++;
-//                 i++;
-//             }
-//             int peak = 1;
-//             while(i < n && arr[i] > arr[i - 1]){
-//                 sum += peak;
-//                 peak += 1;
-//                 i++;
-//             }
-//             int down = 1;
-//             while(i < n && arr[i] < arr[i - 1]){
-//                 sum += down;
-//                 down += 1;
-//                 i++;
-//             }
-//             if(down >= peak){
-//                 sum += (down - peak) + 1;
-//             }
-//         }
-
-//         return sum;
-//     }
-// };
 class Solution {
 public:
     int candy(vector<int>& arr) {
@@ -35,21 +5,20 @@ public:
         vector<int> left(n , 1);
 
         for(int i=1 ; i<n ; i++){
-            if(arr[i] > arr[i - 1]) left[i] = left[i - 1] + 1;
+            if(arr[i - 1] < arr[i]) left[i] = left[i - 1] + 1;
         }
 
-        int cur = 1 , right = 1 , sum = max(left[n - 1] , 1);
+        int right = 1 , cur = 0 , sum = max(1 , left[n - 1]);
         for(int i=n-2 ; i>=0 ; i--){
             if(arr[i] > arr[i + 1]){
-                cur = right + 1;
-                right = cur;
+                int cur = right + 1;
+                right  = cur;
             }
             else{
                 right = 1;
-                cur = 1;
             }
 
-            sum += max(left[i] , cur);
+            sum += max(left[i] , right);
         }
 
         return sum;
