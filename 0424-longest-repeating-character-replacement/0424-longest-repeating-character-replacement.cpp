3c6675b89b5cb719b39ae722c8d258e0fd763dda
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-15 14:45:40 +0530

    Time: 8 ms (35.67%), Space: 11.2 MB (7.37%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n = s.size() , maxlen = 0 , maxfreq = 0 , l = 0 , r = 0;
-        vector<int> mp(26 , 0);
-        while(r < n){
-            mp[s[r] - 'A']++;
-            maxfreq = max(maxfreq , mp[s[r] - 'A']);
-            if( (r - l + 1) - maxfreq > k){
+        unordered_map<int , int> mp;
+        int maxfreq = 0 , l = 0 , ans = 0;
+
+        for(int i=0 ; i<s.size() ; i++){
+            mp[s[i] - 'A']++;
+            maxfreq = max(maxfreq , mp[s[i] - 'A']);
+
+            if((i - l + 1) - maxfreq > k){
                 mp[s[l] - 'A']--;
-                // for(int i=0 ; i<26 ; i++) maxfreq = max(maxfreq , mp[i]);
-                l++; // no need to update max freq as we see the no. of different elemnet k is always taken
-            }// so in order to find better answer we need greater maxfreq so no need to update keep it like that
-            else{
-                maxlen = max(maxlen , r - l + 1);
+                l++;
             }
-            r++;
+            else{
+                ans = max(ans , i - l + 1);
+            }
         }
-        return maxlen;
+
+        return ans;
     }
 };
