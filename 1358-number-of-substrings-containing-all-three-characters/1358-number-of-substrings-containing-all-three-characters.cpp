d540e81ca0e3afa6ebba845dda7b59f594e5b86c
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-15 17:52:21 +0530

    Time: 8 ms (54.93%), Space: 11.2 MB (31.69%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n = s.size() , cnt = 0;
         vector<int> last(3 , -1);
+        int cnt = 0;
 
-        for(int i=0 ; i<n ; i++){
+        for(int i=0 ; i<s.size() ; i++){
             last[s[i] - 'a'] = i;
 
             if(last[0] != -1 && last[1] != -1 && last[2] != -1){
-                cnt += 1 + min(last[0] , min(last[1] , last[2]));
+                cnt += min(last[0] , min(last[1] , last[2])) + 1;
             }
         }
 
