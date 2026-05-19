17f7707555e2de5901ae0288e938f81f903c8cb9
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-19 20:28:48 +0530

    Time: 0 ms (100%), Space: 10.2 MB (33.73%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& arr) {
         int n = arr.size();
-
-        for(int i=0 ; i<=n-2 ; i++){
-            for(int j=i+1 ; j<n ; j++){
+        for(int i=0 ; i<n-1 ; i++){
+            for(int j = i + 1 ; j<n ; j++){
                 swap(arr[i][j] , arr[j][i]);
             }
         }
