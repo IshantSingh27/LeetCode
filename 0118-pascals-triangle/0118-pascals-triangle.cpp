50965fedf700c7ac4ff99092a177b1a5adbe71e8
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-20 11:00:09 +0530

    Time: 11 ms (0.45%), Space: 9.6 MB (88.84%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -5,19 +5,22 @@ public:
         ans.push_back(1);
 
         int cur = 1;
-        for(int col=1 ; col<row ; col++){
+        for(int col=1 ; col<row - 1; col++){
+            cout<<col<<endl;
             cur = cur * (row - col);
             cur = cur / col;
+
             ans.push_back(cur);
         }
 
+        ans.push_back(1);
         return ans;
     }
-
-    vector<vector<int>> generate(int n) {
+    vector<vector<int>> generate(int row) {
         vector<vector<int>> ans;
+        ans.push_back({1});
 
-        for(int i=1 ; i<=n ; i++){
+        for(int i=2 ; i<=row ; i++){
             ans.push_back(gen(i));
         }
 
