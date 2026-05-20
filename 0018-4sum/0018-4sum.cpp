cd7f6ad2d06d3e8042fd95fe945a25962b96a5bd
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-20 16:37:29 +0530

    Time: 16 ms (85.67%), Space: 17.2 MB (98.76%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,29 +2,32 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& arr, int tar) {
         long long n = arr.size();
-        vector<vector<int>> ans;
         sort(arr.begin() , arr.end());
+        vector<vector<int>> ans;
 
-        for(long long i=0 ; i<n ; i++){
+        for(long long i=0 ; i<=n-4 ; i++){
             if(i > 0 && arr[i] == arr[i - 1]) continue;
-
-            for(long long j = i + 1 ; j<n ; j++){
+            long long sum = arr[i];
+            for(long long j = i + 1 ; j<=n-3 ; j++){
                 if(j > i + 1 && arr[j] == arr[j - 1]) continue;
+                long long k = j + 1 , l = n - 1 , cur = sum + arr[j];
 
-                long long k = j + 1 , l = n - 1;
                 while(k < l){
-                    long long sum = arr[i];
-                    sum += arr[j]; sum += arr[k]; sum += arr[l];
+                    long long temp = arr[k] + arr[l];
 
-                    if(sum == tar){
+                    if(cur + temp > tar){
+                        l--;
+                    }
+                    else if(cur + temp < tar){
+                        k++;
+                    }
+                    else{
                         ans.push_back({arr[i] , arr[j] , arr[k] , arr[l]});
                         k++;
                         l--;
                         while(k < l && arr[k] == arr[k - 1]) k++;
-                        while(l > k && arr[l] == arr[l + 1]) l--;
+                        while(k < l && arr[l] == arr[l + 1]) l--;
                     }
-                    else if(sum > tar) l--;
-                    else k++;
                 }
             }
         }
