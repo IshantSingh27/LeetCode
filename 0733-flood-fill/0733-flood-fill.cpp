825c966d3244afa9b3394734d08737f04ddc13df
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-22 08:37:21 +0530

    Time: 0 ms (100%), Space: 18.2 MB (56.65%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    void dfs(int row , int col , vector<vector<int>>& arr , vector<vector<int>>& vis , int color , int old){
-        int n = arr.size() , m = arr[0].size();
-        vis[row][col] = color;
+    void dfs(int row , int col , int old , int color , vector<vector<int>>& arr , int n , int m){
+        arr[row][col] = color;
 
-        vector<int> drow = {-1, 0 , 1 , 0};
-        vector<int> dcol = {0, 1 , 0 , -1};
+        vector<int> drow = {0 , 1 , 0 , -1};
+        vector<int> dcol = {-1 , 0 , 1 , 0};
 
         for(int i=0 ; i<4 ; i++){
             int nrow = row + drow[i];
             int ncol = col + dcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && arr[nrow][ncol] == old && vis[nrow][ncol] != color){
-                dfs(nrow , ncol , arr , vis , color , old);
+
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && arr[nrow][ncol] == old){
+                dfs(nrow , ncol , old , color , arr , n , m);
             }
         }
     }
 
     vector<vector<int>> floodFill(vector<vector<int>>& arr, int sr, int sc, int color) {
         int n = arr.size() , m = arr[0].size() , old = arr[sr][sc];
-        vector<vector<int>> vis = arr;
+        if(old == color) return arr;
+        
+        dfs(sr , sc , old , color , arr , n , m);
 
-        dfs(sr , sc , arr , vis , color , old);
-
-        return vis;
+        return arr;
     }
 };
