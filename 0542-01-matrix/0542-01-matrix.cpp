8cc6d3cb6329a507c8f855a36bc1d7389fd28ed5
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-22 13:03:48 +0530

    Time: 28 ms (24.74%), Space: 38.9 MB (29.88%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& arr) {
         int n = arr.size() , m = arr[0].size();
+        queue<pair<pair<int , int> , int>> q;
         vector<vector<int>> vis(n , vector<int>(m , 0));
         vector<vector<int>> dist(n , vector<int>(m , 0));
-        queue<pair<pair<int , int> , int>> q;
-        vector<int> drow = {-1 , 0 , 1 , 0};
-        vector<int> dcol = {0 , 1 , 0 , -1};
+        vector<int> drow = {0 , 1 , 0 , -1};
+        vector<int> dcol = {-1 , 0 , 1 , 0};
 
         for(int i=0 ; i<n ; i++){
             for(int j=0 ; j<m ; j++){
@@ -20,9 +20,10 @@ public:
         while(!q.empty()){
             int row = q.front().first.first;
             int col = q.front().first.second;
-            int step = q.front().second;
+            int steps = q.front().second;
             q.pop();
-            dist[row][col] = step;
+
+            dist[row][col] = steps;
 
             for(int i=0 ; i<4 ; i++){
                 int nrow = row + drow[i];
@@ -30,7 +31,7 @@ public:
 
                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol]){
                     vis[nrow][ncol] = 1;
-                    q.push({{nrow , ncol} , step + 1});
+                    q.push({{nrow , ncol} , steps + 1});
                 }
             }
         }
