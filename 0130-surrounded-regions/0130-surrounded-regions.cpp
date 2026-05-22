55a2666faf1de9dc31363cb4bfb98ec75b298d36
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-22 13:24:04 +0530

    Time: 7 ms (7.52%), Space: 16 MB (12.9%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     void dfs(int row , int col , vector<vector<int>>& vis , vector<vector<char>>& arr){
         int n = arr.size() , m = arr[0].size();
+        vector<int> drow = {0 , 1 , 0 , -1};
+        vector<int> dcol = {-1 , 0 , 1 , 0};
         vis[row][col] = 1;
-
-        vector<int> drow = {-1 , 0 , 1 , 0};
-        vector<int> dcol = {0 , 1 , 0 , -1};
 
         for(int i=0 ; i<4 ; i++){
             int nrow = row + drow[i];
             int ncol = col + dcol[i];
-
+            
             if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && arr[nrow][ncol] == 'O'){
                 dfs(nrow , ncol , vis , arr);
             }
         }
     }
- 
+
     void solve(vector<vector<char>>& arr) {
         int n = arr.size() , m = arr[0].size();
         vector<vector<int>> vis(n , vector<int>(m , 0));
@@ -24,16 +23,15 @@ public:
         for(int i=0 ; i<n ; i++){
             for(int j=0 ; j<m ; j++){
                 if(i == 0 || j == 0 || i == n-1 || j == m-1){
-                    if(!vis[i][j] && arr[i][j] == 'O'){
+                    if(arr[i][j] == 'O' && !vis[i][j]){
                         dfs(i , j , vis , arr);
                     }
                 }
             }
         }
-
         for(int i=0 ; i<n ; i++){
             for(int j=0 ; j<m ; j++){
-                if(arr[i][j] == 'O' && !vis[i][j]){
+                if(!vis[i][j]){
                     arr[i][j] = 'X';
                 }
             }
