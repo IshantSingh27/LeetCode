f6d5ebcf5e73b8dd30483547dba7bfdc46c0e6a5
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-22 07:39:43 +0530

    Time: 3 ms (27.97%), Space: 17.3 MB (17.68%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& arr) {
-        int n = arr.size() , m = arr[0].size() , fresh = 0 , cnt = 0 , ans = 0;
-        vector<vector<int>> vis(n , vector<int>(m , 0));
+        int n = arr.size() , m = arr[0].size() , cnt = 0 , ans = 0 , t = 0;
         queue<pair<pair<int , int> , int>> q;
+        vector<vector<int>> vis(n , vector<int>(m , 0));
+        vector<int> drow = {0 , 1 , 0 , -1};
+        vector<int> dcol = {-1 , 0 , 1 , 0};
+
         for(int i=0 ; i<n ; i++){
             for(int j=0 ; j<m ; j++){
                 if(arr[i][j] == 2){
                     q.push({{i , j} , 0});
-                    vis[i][j] = 2;
+                    vis[i][j] = 1;
                 }
-                if(arr[i][j] == 1) fresh++;
+                else if(arr[i][j] == 1) cnt++;
             }
         }
-
-        vector<int> drow = {-1 , 0 , 1 , 0};
-        vector<int> dcol = {0 , 1 , 0 , -1};
 
         while(!q.empty()){
             int row = q.front().first.first;
             int col = q.front().first.second;
             int time = q.front().second;
-            ans = max(ans , time);
             q.pop();
 
-            for(int k=0 ; k<4 ; k++){
-                int nrow = row + drow[k];
-                int ncol = col + dcol[k];
-
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol] == 0 && arr[nrow][ncol] != 0){
+            t = max(t , time);
+    
+            for(int i=0 ; i<4 ; i++){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+    
+                if(nrow<n && nrow>=0 && ncol<m && ncol>=0 && !vis[nrow][ncol] && arr[nrow][ncol]==1){
                     q.push({{nrow , ncol} , time + 1});
-                    vis[nrow][ncol] = 2;
-                    cnt++;
+                    vis[nrow][ncol] = 1;
+                    ans++;
                 }
             }
         }
 
-        cout<<"fresh : "<<fresh<<" cnt : "<<cnt<<endl;
-
-        if(cnt == fresh) return ans; 
-        else return -1;
+        if(ans < cnt){
+            return -1;
+        }
+        else{
+            return t;
+        }
     }
 };
