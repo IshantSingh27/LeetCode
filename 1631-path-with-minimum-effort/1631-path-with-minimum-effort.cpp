cce8ad2874ff3dff1068d4d0148fc90b327f04fe
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-23 22:48:14 +0530

    Time: 54 ms (59.07%), Space: 24.5 MB (58.28%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -2,37 +2,35 @@ class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& arr) {
         int n = arr.size() , m = arr[0].size();
-        vector<vector<int>> dist(n , vector<int>(m , 1e9));
-        dist[0][0] = 0;
         priority_queue<pair<int , pair<int , int>> , vector<pair<int , pair<int , int>>> ,
         greater<pair<int , pair<int , int>>>> pq;
         pq.push({0 , {0 , 0}});
+        vector<vector<int>> dist(n , vector<int>(m , 1e9));
+        dist[0][0] = 0;
         vector<int> drow = {1 , 0 , -1 , 0};
         vector<int> dcol = {0 , -1 , 0 , 1};
 
         while(!pq.empty()){
+            int diff = pq.top().first;
             int row = pq.top().second.first;
             int col = pq.top().second.second;
-            int cost = pq.top().first;
             pq.pop();
-
-            if(row == n - 1 && col == m - 1) return dist[n - 1][m - 1];
+            if(row == n - 1 && col == m - 1) return diff;
+            if (diff > dist[row][col]) continue;
 
             for(int i=0 ; i<4 ; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
-
                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
-                    int ncost = max(abs(arr[nrow][ncol] - arr[row][col]) , cost);
-
-                    if(ncost < dist[nrow][ncol]){
-                        dist[nrow][ncol] = ncost;
-                        pq.push({dist[nrow][ncol] , {nrow , ncol}});
+                    int ndif = max(abs(arr[nrow][ncol] - arr[row][col]) , diff);
+                    if(ndif < dist[nrow][ncol]){
+                        dist[nrow][ncol] = ndif;
+                        pq.push({ndif , {nrow , ncol}});
                     }
                 }
             }
         }
 
-        return -1;
+        return 0;
     }
 };
