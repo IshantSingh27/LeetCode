4141bba03f1d5d1d922065a5883305e5d2337d7b
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-23 19:53:43 +0530

    Time: 19 ms (48.98%), Space: 26.4 MB (24.87%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& arr) {
+        if(arr[0][0] == 1) return -1;
         int n = arr.size();
-        if(arr[0][0] != 0 || arr[n - 1][n - 1] != 0) return -1;
-        if(0 == n - 1) return 1;
-
-        vector<vector<int>> dist(n , vector<int>(n , 1e9));
+        vector<vector<int>> vis(n , vector<int>(n , 0));
         queue<pair<int , pair<int , int>>> q;
         q.push({1 , {0 , 0}});
-        vector<int> drow = {1 , 0 , -1 , 0 , 1 , 1 , -1 , -1};
-        vector<int> dcol = {0 , -1 , 0 , 1 , 1 , -1 , 1 , -1};
+        vis[0][0] = 1;
+        vector<int> drow = {0 , 1 , 0 , -1 , 1 , -1 , 1 , -1};
+        vector<int> dcol = {-1 , 0 , 1 , 0 , 1 , -1 , -1 , 1};
 
         while(!q.empty()){
             int row = q.front().second.first;
             int col = q.front().second.second;
             int step = q.front().first;
             q.pop();
+            if(row == n-1 && col == n-1) return step;
 
             for(int i=0 ; i<8 ; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && arr[nrow][ncol] == 0 &&
-                step + 1 < dist[nrow][ncol]){
-                    dist[nrow][ncol] = step + 1;
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && !vis[nrow][ncol] && arr[nrow][ncol] == 0){
                     q.push({step + 1 , {nrow , ncol}});
+                    vis[nrow][ncol] = 1;
                 }
             }
         }
 
-        if(dist[n - 1][n - 1] == 1e9) return -1;
-        return dist[n - 1][n - 1];
+        return -1;
     }
 };
