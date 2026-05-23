7c278525b5441d4d1607bb773e30c5f56b860314
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-24 00:04:11 +0530

    Time: 23 ms (35.98%), Space: 26.2 MB (33.04%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -3,10 +3,10 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& arr) {
         if(arr[0][0] == 1) return -1;
         int n = arr.size();
-        vector<vector<int>> vis(n , vector<int>(n , 0));
+        vector<vector<int>> dist(n , vector<int>(n , 1e9));
         queue<pair<int , pair<int , int>>> q;
         q.push({1 , {0 , 0}});
-        vis[0][0] = 1;
+        dist[0][0] = 1;
         vector<int> drow = {0 , 1 , 0 , -1 , 1 , -1 , 1 , -1};
         vector<int> dcol = {-1 , 0 , 1 , 0 , 1 , -1 , -1 , 1};
 
@@ -15,19 +15,20 @@ public:
             int col = q.front().second.second;
             int step = q.front().first;
             q.pop();
-            if(row == n-1 && col == n-1) return step;
 
             for(int i=0 ; i<8 ; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && !vis[nrow][ncol] && arr[nrow][ncol] == 0){
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && arr[nrow][ncol] == 0 
+                && dist[nrow][ncol] > step + 1){
+                    if(nrow == n-1 && ncol == n - 1) return step + 1;
                     q.push({step + 1 , {nrow , ncol}});
-                    vis[nrow][ncol] = 1;
+                    dist[nrow][ncol] = step + 1;
                 }
             }
         }
-
+        if(dist[n-1][n-1] != 1e9) return dist[n-1][n-1];
         return -1;
     }
 };
