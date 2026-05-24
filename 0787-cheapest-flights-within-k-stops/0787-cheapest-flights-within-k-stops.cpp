f8df9fd236afda25b21087c5813a10e357f3d95c
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-24 18:44:31 +0530

    Time: 0 ms (100%), Space: 18.5 MB (31.57%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& edges, int src, int dst, int k) {
+    int findCheapestPrice(int n, vector<vector<int>>& arr, int src, int dst, int k) {
         vector<vector<pair<int , int>>> adj(n);
-        for(auto it : edges){
+        for(auto it : arr){
             adj[it[0]].push_back({it[1] , it[2]});
         }
-
-        priority_queue<pair<int , pair<int , int>> , vector<pair<int , pair<int , int>>> ,
-        greater<pair<int , pair<int , int>>>> pq;
-        pq.push({0 , {0 , src}});
+        queue<pair<int , pair<int , int>>> q;
+        q.push({0 , {0 , src}});
         vector<int> dist(n , 1e9);
+        dist[src] = 0;
 
-        while(!pq.empty()){
-            int node = pq.top().second.second;
-            int price = pq.top().second.first;
-            int stops = pq.top().first;
-            pq.pop();
-
-            if(stops > k || node == dst) continue;
+        while(!q.empty()){
+            int stops = q.front().first;
+            int cost = q.front().second.first;
+            int node = q.front().second.second;
+            q.pop();
+            if(stops > k) continue;
 
             for(auto it : adj[node]){
                 int adjnode = it.first;
-                int adjprice = it.second;
+                int adjcost = it.second;
 
-                if(price + adjprice < dist[adjnode]){
-                    dist[adjnode] = price + adjprice;
-                    pq.push({stops + 1 , {dist[adjnode] , adjnode}});
+                if(cost + adjcost < dist[adjnode]){
+                    dist[adjnode] = cost + adjcost;
+                    q.push({stops + 1 , {dist[adjnode] , adjnode}});
                 }
             }
         }
 
         if(dist[dst] == 1e9) return -1;
-        return dist[dst];
+        else return dist[dst];
     }
 };
