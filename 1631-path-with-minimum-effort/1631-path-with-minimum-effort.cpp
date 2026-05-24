21f4e13fe1aa8c07c35f0d2321a0de4774ba63d3
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-24 16:15:43 +0530

    Time: 55 ms (57.25%), Space: 24.4 MB (76.96%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -2,35 +2,35 @@ class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& arr) {
         int n = arr.size() , m = arr[0].size();
+        vector<vector<int>> dist(n , vector<int>(m , 1e9));
         priority_queue<pair<int , pair<int , int>> , vector<pair<int , pair<int , int>>> ,
         greater<pair<int , pair<int , int>>>> pq;
         pq.push({0 , {0 , 0}});
-        vector<vector<int>> dist(n , vector<int>(m , 1e9));
         dist[0][0] = 0;
-        vector<int> drow = {1 , 0 , -1 , 0};
-        vector<int> dcol = {0 , -1 , 0 , 1};
+        vector<int> drow = {0 , 1 , 0 , -1};
+        vector<int> dcol = {-1 , 0 , 1 , 0};
 
         while(!pq.empty()){
             int diff = pq.top().first;
             int row = pq.top().second.first;
             int col = pq.top().second.second;
             pq.pop();
-            if(row == n - 1 && col == m - 1) return diff;
-            if (diff > dist[row][col]) continue;
 
             for(int i=0 ; i<4 ; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
+
                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
-                    int ndif = max(abs(arr[nrow][ncol] - arr[row][col]) , diff);
-                    if(ndif < dist[nrow][ncol]){
-                        dist[nrow][ncol] = ndif;
-                        pq.push({ndif , {nrow , ncol}});
+                    int ndif = abs(arr[row][col] - arr[nrow][ncol]);
+                    int ndis = max(ndif , diff);
+                    if(dist[nrow][ncol] > ndis){
+                        dist[nrow][ncol] = ndis;
+                        pq.push({ndis , {nrow , ncol}});
                     }
                 }
             }
         }
 
-        return 0;
+        return dist[n - 1][m - 1];
     }
 };
