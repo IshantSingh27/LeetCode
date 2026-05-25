3ef01bbe9e368dcdc640007e38df7f4347d1f9b3
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-25 21:42:29 +0530

    Time: 9 ms (87.61%), Space: 16.9 MB (55.61%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int cap) {
-        vector<vector<int>> dist(n , vector<int>(n , 1e9));
+        vector<vector<int>> dist(n , vector<int>(n , 1e8));
         for(auto it : edges){
             dist[it[0]][it[1]] = it[2];
             dist[it[1]][it[0]] = it[2];
         }
-
+        for(int i=0 ; i<n ; i++){
+            dist[i][i] = 0;
+        }
         for(int k=0 ; k<n ; k++){
             for(int i=0 ; i<n ; i++){
                 for(int j=0 ; j<n ; j++){
-                    if(dist[i][k] != 1e9 && dist[k][j] != 1e9 && dist[i][k] + dist[k][j] < dist[i][j]){
+                    if(dist[i][k] != 1e8 && dist[k][j] != 1e8 && dist[i][j] > dist[i][k] + dist[k][j]){
                         dist[i][j] = dist[i][k] + dist[k][j];
                     }
                 }
             }
         }
-        
-        int ans = 1e9 , res = 0;
+        int ans = 1e9 , res = -1;
         for(int i=0 ; i<n ; i++){
             int cur = 0;
-            for(int j=0 ; j<n ; j++){
-                if(i == j) continue;
+            for(int j=0; j<n ; j++){
                 if(dist[i][j] <= cap) cur++;
             }
-
             if(cur <= ans){
                 ans = cur;
                 res = i;
             }
         }
-
         return res;
     }
 };
