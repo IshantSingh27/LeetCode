dba6649ea0e7824028bd2fe0eabb0c7b78269ac3
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-28 20:16:33 +0530

    Time: 3 ms (70.08%), Space: 15.5 MB (69.76%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    void solve(int n , int open , string s , vector<string>& ans){
+    void sol(int n , int open , string s , vector<string>& ans){
         if(n == 0){
             while(open != 0){
-                s += ")";
+                s += ')';
                 open--;
             }
             ans.push_back(s);
             return;
         }
 
-        solve(n - 1 , open + 1 , s + "(" , ans);
+        sol(n - 1 , open + 1 , s + '(' , ans);
         if(open > 0){
-            solve(n , open - 1 , s + ")" , ans);
+            sol(n , open - 1 , s + ')' , ans);
         }
-
         return;
     }
-
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
         string s;
-
-        solve(n , 0 , s , ans);
-
+        sol(n , 0 , s , ans);
         return ans;
     }
 };
