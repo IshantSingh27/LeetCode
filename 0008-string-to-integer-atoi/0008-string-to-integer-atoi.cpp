d81e8b082ae244b2be81d51fa90d309c9d8bbb80
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-28 09:08:10 +0530

    Time: 0 ms (100%), Space: 9.1 MB (53.71%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    void solve(long long i , string& s , long long &num){
+    void solve(long long i , string& s , long long &ans){
         if(i >= s.size()) return;
-
         if(s[i] < '0' || s[i] > '9') return;
 
-        num = (num * 10) + (s[i] - '0');
+        ans = (ans * 10) + (s[i] -  '0');
 
-        if(num > (long long)INT_MAX + 1){
-            return;
-        }
+        if(ans > INT_MAX) return;
 
-        solve(i + 1 , s , num);
+        solve(i + 1 , s , ans);
     }
-
     int myAtoi(string s) {
-        long long n = s.size() , i = 0 , sign = 1 , ans = 0;
-        while(i < n){
-            if(s[i] == ' ') i++;
-            else break;
-        }
-        if(i < n && (s[i] == '-' || s[i] == '+')){
-            if(s[i] == '-') sign = -1;
+        long long n = s.size() , ans = 0 , i = 0 , sign = 1;
+        while(i < n && s[i] == ' ') i++;
+        if(s[i] == '-'){
+            sign = -1;
             i++;
         }
-        while(i < n && s[i] == '0') i++;
+        else if(s[i] == '+') i++;
 
         solve(i , s , ans);
 
-        ans = ans * sign;
+        ans *= sign;
 
         if(ans > INT_MAX) return INT_MAX;
         else if(ans < INT_MIN) return INT_MIN;
