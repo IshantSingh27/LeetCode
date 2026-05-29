59e89dc7ef3d5d44d0aa332d50798fd620b13169
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-29 20:12:04 +0530

    Time: 0 ms (100%), Space: 8.6 MB (79.85%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    void sol(int ind , int sum , int n , vector<int>& temp , vector<vector<int>>& ans , int& k){
+    void sol(int ind , int n , vector<int>& temp , vector<vector<int>>& ans , int k){
         if(k == 0){
-            if(sum == n) ans.push_back(temp);
+            if(n == 0) ans.push_back(temp);
             return;
         }
 
         for(int i=ind ; i<=9 ; i++){
-            temp.push_back(i);
-            k--;
-            sol(i + 1 , sum + i , n , temp , ans , k);
-            k++;
-            temp.pop_back();
+            if(i <= n){
+                temp.push_back(i);
+                sol(i + 1 , n - i , temp , ans , k - 1);
+                temp.pop_back();
+            }
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        int sum = 0;
         vector<int> temp;
         vector<vector<int>> ans;
-        sol(1 , sum , n , temp , ans , k);
+        sol(1 , n , temp , ans , k);
         return ans;
     }
 };
