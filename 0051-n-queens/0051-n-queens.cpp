fda32d87e897bd502959a965a76961cac490d2b2
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-30 19:10:02 +0530

    Time: 48 ms (5.89%), Space: 49.8 MB (5%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
-    bool chk(int row , int col , int n , vector<string>& arr){
-        int i = row , j = col;
-        while(j >= 0){
-            if(arr[i][j] == 'Q') return false;
-            j--;
+    bool check(int row , int col , int n , vector<string> temp){
+        if(row == 0) return true;
+
+        for(int i=0 ; i<row ; i++){
+            if(temp[i][col] == 'Q') return false;
         }
-        j = col;
-        while(i >= 0 && j >= 0){
-            if(arr[i][j] == 'Q') return false;
+
+        int i = row - 1 , j = col + 1;
+        while(i >= 0 && j < n){
+            if(temp[i][j] == 'Q') return false;
             i--;
-            j--;
+            j++;
         }
-        i = row;
-        j = col;
-        while(i < n && j >= 0){
-            if(arr[i][j] == 'Q') return false;
-            i++;
+
+        i = row - 1 , j = col - 1;
+        while(i >= 0 && j >= 0){
+            if(temp[i][j] == 'Q') return false;
+            i--;
             j--;
         }
 
         return true;
     }
-
-    void solve(int col , int n , vector<string>& arr , vector<vector<string>>& ans){
-        if(col == n){
-            ans.push_back(arr);
-            return;
+    bool sol(int i , int  n , vector<string>& temp , vector<vector<string>>& ans){
+        if(i == n){
+            ans.push_back(temp);
+            return true;
         }
 
-        for(int i=0 ; i<n ; i++){
-            if(chk(i , col , n , arr)){
-                arr[i][col] = 'Q';
-                solve(col + 1 , n , arr , ans);
-                arr[i][col] = '.';
+        for(int j=0 ; j<n ; j++){
+            if(check(i , j , n , temp)){
+                temp[i][j] = 'Q';
+                if(!sol(i + 1 , n , temp , ans)) return false;
+                temp[i][j] = '.';
             }
         }
+        return true;
     }
-
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> arr(n , string(n , '.'));
-
-        solve(0 , n , arr , ans);
-
+        vector<string> temp(n);
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<n ; j++){
+                temp[i] += '.';
+            }
+        }
+        sol(0 , n , temp , ans);
         return ans;
     }
 };
