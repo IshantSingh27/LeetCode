c3a7829f5092d38385952c7c26974c21e482a871
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-05-31 00:26:37 +0530

    Time: 494 ms (12.1%), Space: 8.8 MB (70.42%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    bool chk(int row , int col , char c , vector<vector<char>>& arr){
+    bool check(int row , int col , char k , vector<vector<char>>& arr){
         for(int i=0 ; i<9 ; i++){
-            if(i != col && arr[row][i] == c) return false;
-
-            if(i != row && arr[i][col] == c) return false;
-
-            if(arr[(3 * (row / 3)) + (i / 3)][(3 * (col / 3)) + (i % 3)] == c) return false;
+            if(i != row && arr[i][col] == k) return false;
+            if(i != col && arr[row][i] == k) return false;
+            if(arr[(3 * (row / 3)) + (i / 3)][(3 * (col / 3)) + (i % 3)] == k) return false;
         }
-
         return true;
     }
-
-    bool solve(vector<vector<char>>& arr){
+    bool sol(vector<vector<char>>& arr){
         for(int i=0 ; i<9 ; i++){
             for(int j=0 ; j<9 ; j++){
                 if(arr[i][j] != '.') continue;
 
                 for(char k='1' ; k<='9' ; k++){
-                    if(chk(i , j , k , arr)){
+                    if(check(i , j , k , arr)){
                         arr[i][j] = k;
-                        if(solve(arr)) return true;
+                        if(sol(arr)) return true;
                         arr[i][j] = '.';
                     }
                 }
@@ -28,11 +24,9 @@ public:
                 return false;
             }
         }
-
         return true;
     }
-
     void solveSudoku(vector<vector<char>>& arr) {
-        solve(arr);
+        sol(arr);
     }
 };
