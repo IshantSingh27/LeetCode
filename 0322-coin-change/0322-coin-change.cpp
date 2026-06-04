ff2370821bac29d9a51c1ec7ce021e776e78b768
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-06-04 16:16:06 +0530

    Time: 43 ms (38.51%), Space: 49 MB (33.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,46 +1,49 @@
 class Solution {
 public:
-    int solve(int ind , int k , vector<int>& arr , vector<vector<int>>& dp){
+    int sol(int ind , int k , vector<int>& arr , vector<vector<int>>& dp){
         if(ind == 0){
-            if(k % arr[0] == 0) return k / arr[0];
-            else return 1e9;
+            if(k == 0) return 0;
+            if(k % arr[ind] == 0) return k / arr[ind];
+            else return 1e8;
         }
 
         if(dp[ind][k] != -1) return dp[ind][k];
 
-        int nottake = solve(ind - 1 , k , arr , dp);
-        int take = 1e9;
-        if(arr[ind] <= k) take = 1 + solve(ind , k - arr[ind] , arr , dp);
+        int nottake = sol(ind - 1 , k , arr , dp);
+        int take = 1e8;
+        if(arr[ind] <= k){
+            take = 1 + sol(ind , k - arr[ind] , arr , dp);
+        }
 
         return dp[ind][k] = min(take , nottake);
     }
-
     int coinChange(vector<int>& arr, int k) {
         int n = arr.size();
         // vector<vector<int>> dp(n , vector<int>(k + 1 , -1));
-
-        // int ans = solve(n - 1 , k , arr , dp);
-        // if(ans >= 1e9) return -1;
+        // int ans = sol(n - 1 , k , arr , dp);
+        // if(ans == 1e8) return -1;
         // else return ans;
-
-        vector<vector<int>> dp(n , vector<int>(k + 1 , 1e9));
-
-        for(int i=0 ; i<=k ; i++){
-            if(i % arr[0] == 0) dp[0][i] = i / arr[0];
+        vector<vector<int>> dp(n , vector<int>(k + 1 , 1e5));
+        for(int i=0 ; i<n ; i++){
+            dp[i][0] = 0;
+        }
+        for(int i=1 ; i<=k ; i++){
+            if(i % arr[0] == 0) dp[0][i] = i / arr[0]; 
         }
 
         for(int i=1 ; i<n ; i++){
             for(int j=0 ; j<=k ; j++){
                 int nottake = dp[i - 1][j];
-                int take = 1e9;
-                if(arr[i] <= j) take = 1 + dp[i][j - arr[i]];
+                int take = 1e8;
+                if(arr[i] <= j){
+                    take = 1 + dp[i][j - arr[i]];
+                }
 
                 dp[i][j] = min(take , nottake);
             }
         }
 
-        int ans = dp[n - 1][k];
-        if(ans >= 1e9) return -1;
-        else return ans;
+        if(dp[n - 1][k] == 1e5) return -1;
+        else return dp[n - 1][k];
     }
 };
