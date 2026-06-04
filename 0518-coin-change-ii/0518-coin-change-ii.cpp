e1abff4843959b95cc859f3e5ec535eb8376bb2c
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-06-04 17:24:06 +0530

    Time: 23 ms (71.15%), Space: 10.5 MB (73.02%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,49 +1,24 @@
 class Solution {
 public:
-    int solve(int ind , int k , vector<int>& arr , vector<vector<int>>& dp){
-        if(ind == 0){
-            if(k == 0 || k % arr[0] == 0) return 1;
-            return 0;
-        }
-
-        if(dp[ind][k] != -1) return dp[ind][k];
-
-        int nottake = solve(ind - 1 , k , arr , dp);
-        int take = 0;
-        if(arr[ind] <= k) take = solve(ind , k - arr[ind] , arr , dp);
-
-        return dp[ind][k] = take + nottake;
-    }
-
     int change(int k, vector<int>& arr) {
-    int n = arr.size();
-
-    vector<vector<long long>> dp(n , vector<long long>(k + 1 , 0));
-
-    if(arr[0] == 0){
-        dp[0][0] = 2;
-    } else {
-        dp[0][0] = 1;
-        for(int i = 1; i <= k; i++){
-            if(i % arr[0] == 0) dp[0][i] = 1;
+        long long n = arr.size() , mod = 1e18 + 7;
+        vector<long long> prev(k + 1 , 0);
+        for(long long i=0 ; i<=k ; i++){
+            if(i % arr[0] == 0) prev[i] = 1; 
         }
-    }
 
-    const int MOD = 1e18 + 7;
+        for(long long i=1 ; i<n ; i++){
+            for(long long j=0 ; j<=k ; j++){
+                long long nottake = prev[j];
+                long long take = 0;
+                if(arr[i] <= j){
+                    take = prev[j - arr[i]];
+                }
 
-    for(int i = 1 ; i < n ; i++){
-        for(int j = 0 ; j <= k ; j++){
-
-            long long nottake = dp[i - 1][j];
-
-            long long take = 0;
-            if(arr[i] <= j)
-                take = dp[i][j - arr[i]];
-
-            dp[i][j] = (take + nottake) % MOD;
+                prev[j] = (take + nottake) % mod;
+            }
         }
-    }
 
-    return dp[n - 1][k];
-}
+        return prev[k] % mod;
+    }
 };
