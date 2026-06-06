dec88948540cb6d6096cf58f9397c7c26d456a6b
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-06-06 11:48:05 +0530

    Time: 0 ms (100%), Space: 20.2 MB (38.78%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -22,18 +22,33 @@ public:
         // vector<vector<int>> dp(n , vector<int>(2 , -1));
         // return sol(0 , 1 , arr , dp);
 
-        vector<vector<int>> dp(n + 1 , vector<int>(2 , 0));
+        // vector<vector<int>> dp(n + 1 , vector<int>(2 , 0));
+
+        // for(int i=n-1 ; i>=0 ; i--){
+        //     for(int j=0 ; j<2 ; j++){
+        //         int profit = 0;
+        //         if(j){
+        //             profit = max(dp[i + 1][j] , dp[i + 1][0] - arr[i]);
+        //         }
+        //         else profit = max(dp[i + 1][j] , dp[i + 1][1] + arr[i]);
+        //         dp[i][j] = profit;
+        //     }
+        // }
+        // return dp[0][1];
+
+        vector<int> prev(2 , 0) , cur(2 , 0);
 
         for(int i=n-1 ; i>=0 ; i--){
             for(int j=0 ; j<2 ; j++){
                 int profit = 0;
                 if(j){
-                    profit = max(dp[i + 1][j] , dp[i + 1][0] - arr[i]);
+                    profit = max(prev[j] , prev[0] - arr[i]);
                 }
-                else profit = max(dp[i + 1][j] , dp[i + 1][1] + arr[i]);
-                dp[i][j] = profit;
+                else profit = max(prev[j] , prev[1] + arr[i]);
+                cur[j] = profit;
             }
+            prev = cur;
         }
-        return dp[0][1];
+        return prev[1];
     }
 };
