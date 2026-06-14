6030e4a9880dc8545b09e8da1d4b02229c6d2aa5
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-06-14 15:46:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (42.41%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    double sol(double x , int n){
+    double sol(double x, long long n){
         if(n == 0) return 1;
 
         double half = sol(x , n / 2);
 
-        if(n % 2 == 0) return half * half;
-        else return half * half * x;
+        if(n % 2 == 1) return half * half * x;
+        else return half * half;
     }
     double myPow(double x, int n) {
+        if(x == 1) return 1;
+        long long p = n;
         if(n < 0){
             x = 1 / x;
-            n = (long long)(-1) * n;
+            p = (long long)(-1) * p;
         }
-        if(x == 1) return 1;
-        return sol(x , n);
+        
+        return sol(x , p);
     }
 };
