17db0aa624d62fb59a7d3f99238519e6361a0fa9
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-06-23 16:23:56 +0530

    Time: 496 ms (12.34%), Space: 8.7 MB (71%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    bool check(int row , int col , char k , vector<vector<char>>& arr){
+    bool chk(int row , int col , char k , vector<vector<char>>& arr){
         for(int i=0 ; i<9 ; i++){
             if(i != row && arr[i][col] == k) return false;
             if(i != col && arr[row][i] == k) return false;
-            if(arr[(3 * (row / 3)) + (i / 3)][(3 * (col / 3)) + (i % 3)] == k) return false;
+            if(arr[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == k) return false;
         }
         return true;
     }
@@ -12,15 +12,13 @@ public:
         for(int i=0 ; i<9 ; i++){
             for(int j=0 ; j<9 ; j++){
                 if(arr[i][j] != '.') continue;
-
                 for(char k='1' ; k<='9' ; k++){
-                    if(check(i , j , k , arr)){
+                    if(chk(i , j , k , arr)){
                         arr[i][j] = k;
                         if(sol(arr)) return true;
                         arr[i][j] = '.';
                     }
                 }
-
                 return false;
             }
         }
