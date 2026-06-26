d424e724fae3543cf297252d877df8ad44e7980b
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-06-27 02:03:59 +0530

    Time: 102 ms (7.03%), Space: 217.4 MB (5.02%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,52 +1,51 @@
 class Solution {
 public:
-    vector<int> nextsmall(vector<int>& arr){
-        int n = arr.size();
-        vector<int> ans(n);
-        stack<int> st;
+long long mod = 1e9 + 7;
+    vector<long long> nxt(vector<int>& arr){
+        long long n = arr.size();
+        vector<long long> ans(n , n);
+        stack<long long> st;
 
-        for(int i=n-1 ; i>=0 ; i--){
-            while(!st.empty() && arr[st.top()] >= arr[i]){
+        for(long long i=n-1 ; i>=0 ; i--){
+            while(!st.empty() && arr[i] <= arr[st.top()]){
                 st.pop();
             }
-            if(st.empty()) ans[i] = n;
-            else ans[i] = st.top();
+
+            if(!st.empty()){
+                ans[i] = st.top();
+            }
 
             st.push(i);
         }
-
         return ans;
     }
+    vector<long long> pre(vector<int>& arr){
+        long long n = arr.size();
+        vector<long long> ans(n , -1);
+        stack<long long> st;
 
-    vector<int> prevsmall(vector<int>& arr){
-        int n = arr.size();
-        vector<int> ans(n);
-        stack<int> st;
-
-        for(int i=0 ; i<n ; i++){
-            while(!st.empty() && arr[st.top()] > arr[i]){
+        for(long long i=0 ; i<n ; i++){
+            while(!st.empty() && arr[i] < arr[st.top()]){
                 st.pop();
             }
-            if(st.empty()) ans[i] = -1;
-            else ans[i] = st.top();
+
+            if(!st.empty()){
+                ans[i] = st.top();
+            }
 
             st.push(i);
         }
-
         return ans;
     }
-
     int sumSubarrayMins(vector<int>& arr) {
-        vector<int> nse = nextsmall(arr);
-        vector<int> pse = prevsmall(arr);
-        int n = arr.size() , tot = 0 , mod = 1e9 + 7;
+        vector<long long> nse = nxt(arr);
+        vector<long long> pse = pre(arr);
 
-        for(int i=0 ; i<n ; i++){
-            long long right = (nse[i] - i);
-            long long left = (i - pse[i]);
-            tot = (tot + ((right * left) % mod * arr[i] * 1LL) % mod) % mod;
+        long long tot = 0;
+        for(long long i=0 ; i<arr.size() ; i++){
+            tot = tot + (( ((nse[i] - i) * (i - pse[i]) % mod) * arr[i] * 1LL) % mod) % mod;
         }
 
-        return tot;
+        return tot % mod;
     }
 };
