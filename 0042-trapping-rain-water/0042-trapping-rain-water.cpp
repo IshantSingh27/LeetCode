5fef863dbd75b2b879dcce480a1e49d63c6ebf2e
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-06-27 13:32:48 +0530

    Time: 0 ms (100%), Space: 25.9 MB (77.88%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int trap(vector<int>& arr) {
-        int n = arr.size() , l = 0 , r = n - 1 , lmax = 0 , rmax = 0 , tot = 0;
-
+        int l = 0 , r = arr.size() - 1 , lmax = 0 , rmax = 0 , tot = 0;
         while(l < r){
             if(arr[l] <= arr[r]){
-                if(lmax >= arr[l]){
+                if(arr[l] < lmax){
                     tot += lmax - arr[l];
                 }
                 else lmax = arr[l];
                 l++;
             }
             else{
-                if(rmax >= arr[r]){
+                if(arr[r] < rmax){
                     tot += rmax - arr[r];
                 }
                 else rmax = arr[r];
                 r--;
             }
         }
-
         return tot;
     }
 };
