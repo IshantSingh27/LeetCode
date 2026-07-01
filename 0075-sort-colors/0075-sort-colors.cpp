4085ec516ac4bfe044a76f861fd22a778158480d
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-07-02 01:38:33 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& arr) {
-        int n = arr.size() , l = 0 , r = n - 1 , i = 0;
-
-        while(i <= r){
-            // cout<<"arr[i] : "<<arr[i]<<" | ";
-            if(arr[i] == 0){
-                // cout<<"here 1 ";
-                swap(arr[i] , arr[l]);
-                l++;
+        int n = arr.size() , i = 0 , k = 0 , j = n - 1;
+        while(k <= j){
+            if(arr[k] == 0){
+                swap(arr[k] , arr[i]);
                 i++;
+                k++;
             }
-            else if(arr[i] == 2){
-                swap(arr[i] , arr[r]);
-                r--;
+            else if(arr[k] == 2){
+                swap(arr[j] , arr[k]);
+                j--;
             }
-            else i++;
-
-        //     for(int j=0 ; j<n ; j++){
-        //     cout<<arr[j]<<" ";
-        // }
-        // cout<<endl;
+            else{
+                k++;
+            }
         }
     }
 };
