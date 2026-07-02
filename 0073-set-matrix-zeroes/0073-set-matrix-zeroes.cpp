83263aa835c5287a4f66f167046117168473da96
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-07-02 17:14:41 +0530

    Time: 0 ms (100%), Space: 20.8 MB (76.84%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,28 +4,26 @@ public:
         int n = arr.size() , m = arr[0].size() , col0 = 1;
         for(int i=0 ; i<n ; i++){
             for(int j=0 ; j<m ; j++){
-                if(arr[i][j] == 0){
-                    arr[i][0] = 0;
-
-                    if(j != 0){
+                if(j != 0){
+                    if(arr[i][j] == 0){
+                        arr[i][0] = 0;
                         arr[0][j] = 0;
                     }
-                    else{
+                }
+                else{
+                    if(arr[i][0] == 0){
                         col0 = 0;
                     }
                 }
             }
         }
-
         for(int i=1 ; i<n ; i++){
             for(int j=1 ; j<m ; j++){
-                if(arr[i][j] == 0) continue;
                 if(arr[i][0] == 0 || arr[0][j] == 0){
                     arr[i][j] = 0;
                 }
             }
         }
-
         if(arr[0][0] == 0){
             for(int i=0 ; i<m ; i++){
                 arr[0][i] = 0;
