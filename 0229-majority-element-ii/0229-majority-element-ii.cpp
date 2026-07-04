cda33efdc0b2f787aec2b470894448b8114d1a49
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-07-04 17:00:29 +0530

    Time: 0 ms (100%), Space: 23.5 MB (83.5%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& arr) {
-        int cnt1 = 0 , cnt2 = 0 , ele1 , ele2 , n = arr.size();
-
+        int a , b , n = arr.size() , cnta = 0 , cntb = 0;
         for(int i=0 ; i<n ; i++){
-            if(cnt1 == 0 && arr[i] != ele2){
-                ele1 = arr[i];
-                cnt1++;
+            if(cnta == 0 && arr[i] != b){
+                cnta++;
+                a = arr[i];
             }
-            else if(cnt2 == 0 && arr[i] != ele1){
-                ele2 = arr[i];
-                cnt2++;
+            else if(arr[i] == a) cnta++;
+            else if(cntb == 0){
+                cntb++;
+                b = arr[i];
             }
-            else if(arr[i] == ele1){
-                cnt1++;
-            }
-            else if(arr[i] == ele2){
-                cnt2++;
-            }
+            else if(arr[i] == b) cntb++;
             else{
-                cnt1--;
-                cnt2--;
+                cnta--;
+                cntb--;
             }
-
-            // cout<<ele1<<" | "<<ele2<<endl;
         }
-
-        cnt1 = 0 , cnt2 = 0;
+        cnta = 0;
+        cntb = 0;
         for(int i=0 ; i<n ; i++){
-            if(arr[i] == ele1) cnt1++;
-            else if(arr[i] == ele2) cnt2++;
+            if(arr[i] == a) cnta++;
+            else if(arr[i] == b) cntb++;
         }
-        if(cnt1 > n/3 && cnt2 > n/3) return {ele1 , ele2};
-        else if(cnt1 > n/3) return {ele1};
-        else if(cnt2 > n/3) return {ele2};
+        if(cnta > n / 3 && cntb > n / 3) return {a , b};
+        else if(cnta > n / 3) return {a};
+        else if(cntb > n / 3) return {b};
         else return {};
     }
 };
