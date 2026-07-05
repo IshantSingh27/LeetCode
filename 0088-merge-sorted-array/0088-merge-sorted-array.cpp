5059bd7e45b6b9de41dc58422108e8ce712210c4
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-07-05 23:31:47 +0530

    Time: 0 ms (100%), Space: 12.4 MB (42.04%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    void merge(vector<int>& arr1, int n, vector<int>& arr2, int m) {
-        int i = n - 1;
-        int j = m - 1;
-        int k = n + m - 1;
-
-        while(i >= 0 && j >= 0){
-            if(arr1[i] > arr2[j]){
-                arr1[k] = arr1[i];
-                k--;
-                i--;
+void sol(vector<int>& nums1, vector<int>& nums2, int left , int right){
+    if(nums1[left] > nums2[right]){
+        swap(nums1[left] , nums2[right]);
+    }
+}
+    void merge(vector<int>& nums1, int n, vector<int>& nums2, int m) {
+        int gap = (n + m) / 2 + (n + m) % 2;
+        while(gap > 0){
+            int left = 0;
+            int right = left + gap;
+            while(right < n + m){
+                if(left < n && right >= n){
+                    sol(nums1 , nums2 , left , right - n);
+                }
+                else if(left >= n){
+                    sol(nums2 , nums2 , left - n , right - n);
+                }
+                else{
+                    sol(nums1 , nums1 , left , right);
+                }
+                left++;
+                right++;
             }
-            else{
-                arr1[k] = arr2[j];
-                k--;
-                j--;
-            }
+            if(gap == 1) break;
+            gap = (gap / 2) + (gap % 2);
         }
-
-        while(j >= 0){
-            arr1[k] = arr2[j];
-            k--;
-            j--;
+        for(int i=n ; i<n + m ; i++){
+            nums1[i] = nums2[i - n];
         }
     }
 };
