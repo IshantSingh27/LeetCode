80f7a283d6feae557f4c8f255f05de565f1cd022
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-07-12 17:02:59 +0530

    Time: 0 ms (100%), Space: 8.5 MB (30.88%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int n = s.size() , open = 0 , cnt = 0;
+        for(int i=0 ; i<n ; i++){
+            if(s[i] == '(') open++;
+            else open--;
+            if(open < 0){
+                cnt++;
+                open = 0;
+            }
+        }
+        cnt += open;
+
+        return cnt;
+    }
+};
