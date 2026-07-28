b62faad30f7402fa60228238df3a2a0497d0fadf
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-07-28 10:48:01 +0530

    Time: 0 ms (100%), Space: 12.5 MB (72.92%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -6,15 +6,16 @@ public:
         else if(arr[0] > arr[1]) return 0;
         else if(arr[n - 1] > arr[n - 2]) return n - 1;
 
-        int s = 1 , e = n - 2;
-        while(s <= e){
-            int mid = s + (e - s) / 2;
+        int s = 1 , e = n - 2 , mid = s + (e - s) / 2;;
+        while(s < e){
             if(arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) return mid;
 
             if(arr[mid] < arr[mid + 1] && arr[mid] > arr[mid - 1]) s = mid + 1;
             else e = mid - 1;
+
+            mid = s + (e - s) / 2;
         }
 
-        return -1;
+        return mid;
     }
 };
