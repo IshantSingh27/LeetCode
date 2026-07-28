e2f5e428944c4885c50c7b17af0953fd6a5b1c5c
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-07-28 11:45:17 +0530

    Time: 0 ms (100%), Space: 26.3 MB (35.77%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        int s = 0 , e = n - 1;
         if(n == 1) return nums[0];
         if(nums[0] != nums[1]) return nums[0];
-        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        if(nums[n - 1] != nums[n - 2]) return nums[n - 1];
 
+        int s = 1 , e = n - 2;
         while(s <= e){
             int mid = s + (e - s) / 2;
-
-            if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]){
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]){
                 return nums[mid];
             }
-            else if( (mid%2 == 0 && nums[mid] == nums[mid + 1]) || 
-            (mid%2 == 1 && nums[mid] == nums[mid - 1]) ){
-                s = mid + 1;
-            }
-            else{
-                e = mid - 1;
-            }
+
+            if(mid % 2 == 0 && nums[mid] != nums[mid + 1]) e = mid - 1;
+            else if(mid % 2 == 1 && nums[mid] != nums[mid - 1]) e = mid - 1;
+            else s = mid + 1;
+
+            cout<<"s : "<<s<<" e : "<<e<<endl;
         }
 
         return -1;
