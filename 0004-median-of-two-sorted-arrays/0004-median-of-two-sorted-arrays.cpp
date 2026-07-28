da3d7dea7b6f3e6500f8c2b0e022123e307165db
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-07-28 20:01:36 +0530

    Time: 0 ms (100%), Space: 95 MB (80.09%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,72 +1,24 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0 , j = 0 , n = nums1.size() , m = nums2.size() , cnt = 0;
-        int med = n + m , t1 = 0;
-        double ans;
-        if((n + m) % 2 == 0){
-            t1 = (n + m) / 2;
-        }
-        else{
-            t1 = ((n + m) + 1) / 2;
-        }
-        while(i < n && j < m){
-            if(nums1[i] <= nums2[j]){
-                cnt++;
-                if(cnt == t1){
-                    ans = nums1[i];
-                    i++;
-                    break;
-                }
-                i++;
-            }
-            else if(nums1[i] >= nums2[j]){
-                cnt++;
-                if(cnt == t1){
-                    ans = nums2[j];
-                    j++;
-                    break;
-                }
-                j++;
-            }
-        }
-        if(cnt != t1){
-            while(i < n){
-                cnt++;
-                if(cnt == t1){
-                    ans = nums1[i];
-                    i++;
-                    break;
-                }
-                i++;
-            }
-            while(j < m){
-                cnt++;
-                if(cnt == t1){
-                    ans = nums2[j];
-                    j++;
-                    break;
-                }
-                j++;
-            }
-        }
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+        int n = a.size(), m = b.size();
+    int total = n + m;
 
-        if((n + m) % 2 == 0){
-            double cur = 0;
-            if(i < n && j < m && nums1[i] <= nums2[j]){
-                cur = nums1[i];
-            }
-            else if(i < n && j < m && nums1[i] > nums2[j]){
-                cur = nums2[j];
-            }
-            else if(i < n) cur = nums1[i];
-            else cur = nums2[j];
+    int i = 0, j = 0;
+    int prev = 0, curr = 0;
 
-            double res = (cur + ans) / 2;
-            return res;
-        }
-        else{
-            return ans;
-        }
+    for (int cnt = 0; cnt <= total / 2; cnt++) {
+        prev = curr;
+
+        if (i < n && (j >= m || a[i] <= b[j]))
+            curr = a[i++];
+        else
+            curr = b[j++];
+    }
+
+    if (total % 2)
+        return curr;
+
+    return (prev + curr) / 2.0;
     }
 };
