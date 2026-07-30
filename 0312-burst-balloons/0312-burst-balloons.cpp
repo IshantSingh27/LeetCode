eecd975f960c42a2248797b9b2d33c1f548443ff
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-07-30 13:38:36 +0530

    Time: 95 ms (79.46%), Space: 13.7 MB (91.6%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -19,8 +19,23 @@ public:
         arr.push_back(1);
         arr.insert(arr.begin(), 1);
         int n = arr.size();
-        vector<vector<int>> dp(n , vector<int>(n , -1));
+        // vector<vector<int>> dp(n , vector<int>(n , -1));
+        // return sol(1 , n - 2 , arr , dp);
 
-        return sol(1 , n - 2 , arr , dp);
+        vector<vector<int>> dp(n , vector<int>(n , 0));
+
+        for(int i=n-2 ; i>=1 ; i--){
+            for(int j=i ; j<=n-2 ; j++){
+                int maxi = -1e9;
+                for(int k=i ; k<=j ; k++){
+                    int cost = arr[i - 1] * arr[k] * arr[j + 1] + dp[i][k - 1] + dp[k + 1][j];
+
+                    maxi = max(maxi , cost);
+                }
+
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n-2];
     }
 };
