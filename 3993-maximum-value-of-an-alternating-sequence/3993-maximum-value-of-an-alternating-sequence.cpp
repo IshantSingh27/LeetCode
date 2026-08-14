13ca91f2e589fd7336986af2994512558e06cc1d
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-08-14 10:33:23 +0530

    Time: 0 ms (100%), Space: 9.1 MB (75.25%) - LeetHub

---
+++ 3993-maximum-value-of-an-alternating-sequence/3993-maximum-value-of-an-alternating-sequence.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long maximumValue(int n, int s, int m) {
+        if(n == 1) return s;
+        
+        long long k = n / 2;
+
+        return 1LL * s + k * m - (k - 1);
+    }
+};
