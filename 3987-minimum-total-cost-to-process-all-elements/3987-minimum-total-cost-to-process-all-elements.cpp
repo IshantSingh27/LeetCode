f8afa8f26430425f651c9b1ca459ded60fd0add6
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-08-18 14:11:43 +0530

    Time: 11 ms (45.82%), Space: 169.4 MB (34.49%) - LeetHub

--- 3987-minimum-total-cost-to-process-all-elements/3987-minimum-total-cost-to-process-all-elements.cpp
+++ 3987-minimum-total-cost-to-process-all-elements/3987-minimum-total-cost-to-process-all-elements.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     int minimumCost(vector<int>& nums, int k) {
-        const long long MOD = 1e9 + 7;
+        long long cnt = 0 , cur = k , mod = 1e9 + 7;
 
-        long long cur = k;   // currently available capacity
-        long long cnt = 0;   // number of extra operations
-
-        for (long long x : nums) {
-
-            if (x > cur) {
-                // Number of extra k-sized operations needed
+        for(auto x : nums){
+            if(x > cur){
                 long long need = (x - cur + k - 1LL) / k;
 
                 cnt += need;
                 cur += need * k;
             }
 
-            // Process this element
             cur -= x;
         }
 
-        cnt %= MOD;
+        cnt = cnt % mod;
 
-        // 1 + 2 + ... + cnt
-        return (int)((cnt * (cnt + 1) / 2) % MOD);
+        return (int)(((cnt * (cnt + 1)) / 2) % mod);
     }
 };
