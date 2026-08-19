623a169329d11b51dd6a9ef3ca3aa05aadfe290d
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-08-19 19:28:23 +0530

    Time: 28 ms (17.27%), Space: 46 MB (5.7%) - LeetHub

--- 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
+++ 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
@@ -1,79 +1,42 @@
 class Solution {
 public:
     bool canMakeSubsequence(string s, string t) {
-        int n = s.size();
-        int m = t.size();
+        int n = s.size() , m = t.size();
+        vector<int> pre(n , m) , suf(n , -1);
 
-        if (n > m)
-            return false;
+        int p = 0;
+        for(int i=0 ; i<n ; i++){
+            while(p < m && t[p] != s[i]) p++;
 
-        const int INF = m + 1;
-
-        // nxt[i][c] = first position >= i
-        // where character c occurs in t
-        vector<vector<int>> nxt(m + 1, vector<int>(26, INF));
-
-        for (int i = m - 1; i >= 0; i--) {
-            nxt[i] = nxt[i + 1];
-            nxt[i][t[i] - 'a'] = i;
-        }
-
-        // Position in t after matching s prefix
-        // without replacement / with replacement
-        int p0 = 0;
-        int p1 = 0;
-
-        for (char c : s) {
-
-            int x = c - 'a';
-
-            // -------------------------
-            // Don't use replacement
-            // -------------------------
-            int pos0 = INF;
-
-            if (p0 < m) {
-                int idx = nxt[p0][x];
-
-                if (idx < m)
-                    pos0 = idx + 1;
+            if(p == m){
+                break;
             }
 
-            // -------------------------
-            // Already used replacement
-            // OR use replacement now
-            // -------------------------
-
-            // Case 1:
-            // replacement was already used,
-            // so current character must match normally.
-            int exact = INF;
-
-            if (p1 < m) {
-                int idx = nxt[p1][x];
-
-                if (idx < m)
-                    exact = idx + 1;
-            }
-
-            // Case 2:
-            // Use replacement on current character.
-            //
-            // We can change s[i] into ANY character,
-            // so change it into t[p0].
-            int replace = INF;
-
-            if (p0 < m)
-                replace = p0 + 1;
-
-            int pos1 = min(exact, replace);
-
-            p0 = pos0;
-            p1 = pos1;
+            pre[i] = p++;
         }
 
-        // Either we succeeded without replacement
-        // or with one replacement.
-        return p0 <= m || p1 <= m;
+        if (pre[n - 1] != m) return true;
+
+        p = m - 1;
+        for(int i=n - 1 ; i>=0 ; i--){
+            while(p >= 0 && t[p] != s[i]) p--;
+
+            if(p < 0) break;
+
+            suf[i] = p--;
+        }
+
+        for(int i=0 ; i<n ; i++){
+            int left , right;
+            if(i==0) left = -1;
+            else left = pre[i - 1];
+            if(i == n - 1) right = m;
+            else right = suf[i + 1];
+
+            if(left != m && right != -1){
+                if(left + 1 < right) return true;
+            }
+        }
+        return false;
     }
 };
