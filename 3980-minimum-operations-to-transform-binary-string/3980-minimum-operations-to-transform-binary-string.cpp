934001674eff4a2b3d3d2ae9e10b72a1ccb63b65
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-08-21 18:24:57 +0530

    Time: 16 ms (49.16%), Space: 30.3 MB (79.93%) - LeetHub

---
+++ 3980-minimum-operations-to-transform-binary-string/3980-minimum-operations-to-transform-binary-string.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minOperations(string s1, string s2) {
+        int n = s1.size() , ans = 0;
+
+        if(n == 1){
+            if(s1[0] != s2[0]){
+                if(s1[0] == '1') return -1;
+                else return 1;
+            }
+            else return 0;
+        }
+
+        for(int i=0 ; i<n - 1 ; i++){
+            if(s1[i] == s2[i]) continue;
+            else{
+                if(s1[i] == '1'){
+                    if(i < n - 1 && s1[i + 1] == '1'){
+                        ans++;
+                        s1[i] = '0';
+                        s1[i + 1] = '0';
+                    }
+                    else{
+                        ans += 2;
+                        s1[i] = '0';
+                    }
+                }
+                else{
+                    ans++;
+                    s1[i] = '1';
+                }
+            }
+        }
+
+        if(s1[n - 1] == s2[n - 1]) return ans;
+        else{
+            if(s1[n - 1] == '0') return ans + 1;
+            else{
+                return ans + 2;
+            }
+        }
+    }
+};
