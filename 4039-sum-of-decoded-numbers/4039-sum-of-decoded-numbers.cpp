ae8aea691a8b4df996bd86e19d5abb830b4f85ec
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-08-31 21:17:04 +0530

    Time: 111 ms (71.43%), Space: 90.7 MB (100%) - LeetHub

--- 4039-sum-of-decoded-numbers/4039-sum-of-decoded-numbers.cpp
+++ 4039-sum-of-decoded-numbers/4039-sum-of-decoded-numbers.cpp
@@ -1,44 +1,37 @@
 class Solution {
 public:
+    long long mod = 1e9 + 7;
 
-    long long power(long long x, long long y, long long mod) {
-
-        long long result = 1;
-
+    long long power(long long x , long long y){
+        long long res = 1;
         x %= mod;
 
-        while(y > 0) {
-
-            if(y & 1)
-                result = (result * x) % mod;
+        while(y > 0){
+            if(y & 1) res = (res * x) % mod;
 
             x = (x * x) % mod;
 
-            y /= 2;
+            y = y / 2;
         }
 
-        return result;
+        return res;
     }
-
     int sumDecoded(vector<long long>& nums) {
-
-        const long long mod = 1e9 + 7;
         long long ans = 0;
 
-        for(long long num : nums) {
-
-            long long width = num % 10;
-            long long d = num / 10;
+        for(long long i=0 ; i<nums.size() ; i++){
+            long long width = nums[i] % 10;
+            long long d = nums[i] / 10;
 
             string s = to_string(d);
 
-            string sx = s.substr(0, width);
+            string sx = s.substr(0 , width);
             string sy = s.substr(width);
 
             long long x = stoll(sx);
             long long y = stoll(sy);
 
-            ans = (ans + power(x, y, mod)) % mod;
+            ans = (ans + power(x , y)) % mod;
         }
 
         return ans;
