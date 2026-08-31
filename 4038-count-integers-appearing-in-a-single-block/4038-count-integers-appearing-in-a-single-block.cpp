5f510b7453e13140c05a48dd0f6c11b64c3fac60
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-08-31 12:04:27 +0530

    Time: 2 ms (62.5%), Space: 20.9 MB (100%) - LeetHub

--- 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.cpp
+++ 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int countSpecialIntegers(vector<int>& nums) {
-        unordered_map<int , int> mp;
-        int n = nums.size() , cnt = 0;
-
-        for(int i=0 ; i<n ; i++){
-            if(i > 0 && nums[i] != nums[i - 1] && mp.find(nums[i]) != mp.end() && mp[nums[i]] != 2){
-                cnt++;
-                mp[nums[i]] = 2;
-
-            }
-            else if((i == 0 || nums[i] != nums[i - 1]) && mp[nums[i]] != 2) mp[nums[i]] = 1;
+        vector<int> block(101 , 0);
+        for(int i=0 ; i<nums.size() ; i++){
+            if(i == 0 || nums[i] != nums[i - 1]) block[nums[i]]++;
         }
 
-        return max(0 , (int)mp.size() - cnt);
+        int ans = 0;
+        for(int i=1 ; i<101 ; i++){
+            if(block[i] == 1) ans++;
+        }
+
+        return ans;
     }
 };
