1d9cf8c09c1279c87b4f91ed20fdcb54d38b51da
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-09-01 01:59:55 +0530

    Time: 191 ms (93.94%), Space: 69.6 MB (43.05%) - LeetHub

--- 4040-minimum-operations-to-form-subset-sum-i/4040-minimum-operations-to-form-subset-sum-i.cpp
+++ 4040-minimum-operations-to-form-subset-sum-i/4040-minimum-operations-to-form-subset-sum-i.cpp
@@ -1,67 +1,46 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int sum) {
-
-        const int INF = 1e9;
-
-        vector<int> dp(sum + 1, INF);
+        vector<int> dp(sum + 1 , INT_MAX);
         dp[0] = 0;
 
-        for (int x : nums) {
+        for(int x : nums){
+            vector<pair<int , int>> options;
 
-            // value -> minimum operations needed
-            vector<pair<int, int>> options;
+            options.push_back({x , 0});
 
-            // Keep x as it is
-            options.push_back({x, 0});
-
-            // Divide: x/2, x/4, x/8, ...
-            int val = x;
-            int cost = 0;
-
-            while (val > 0) {
-                val /= 2;
+            int cost = 0 , val = x;
+            while(val > 0){
+                val = val / 2;
                 cost++;
 
-                if (val == 0)
-                    break;
+                if(val == 0) break;
 
-                options.push_back({val, cost});
+                options.push_back({val , cost});
             }
 
-            // Multiply: 2*x, 4*x, 8*x, ...
-            val = x;
-            cost = 0;
-
-            while (val <= sum / 2) {
-                val *= 2;
+            cost = 0 ; val = x;
+            while(val <= sum / 2){
+                val = val * 2;
                 cost++;
 
-                options.push_back({val, cost});
+                options.push_back({val , cost});
             }
 
-            // 0/1 knapsack transition
             vector<int> ndp = dp;
+            for(int s=0 ; s<=sum ; s++){
+                if(dp[s] == INT_MAX) continue;
 
-            for (int s = 0; s <= sum; s++) {
+                for(auto [value , operations] : options){
+                    if(s + value > sum) continue;
 
-                if (dp[s] == INF)
-                    continue;
-
-                for (auto [value, operations] : options) {
-
-                    if (s + value > sum)
-                        continue;
-
-                    ndp[s + value] =
-                        min(ndp[s + value],
-                            dp[s] + operations);
+                    ndp[s + value] = min(ndp[s + value] , dp[s] + operations);
                 }
             }
-
             dp = ndp;
         }
 
-        return dp[sum] == INF ? -1 : dp[sum];
+        return dp[sum] == INT_MAX ? -1 : dp[sum];
+
     }
 };
