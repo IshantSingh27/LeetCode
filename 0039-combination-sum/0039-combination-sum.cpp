a3b01712ed17ba9f233ae6fee84f2e962f80beb3
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-09-24 19:48:53 +0530

    Time: 0 ms (100%), Space: 18.3 MB (18.79%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    void sol(int i , int tar , vector<int>& temp , vector<vector<int>>& ans , vector<int>& arr){
-        if(tar == 0){
-            ans.push_back(temp);
+    void sol(int ind , int tar , vector<int>& arr , vector<int>& temp , vector<vector<int>>& ans){
+        if(ind < 0){
+            if(tar == 0) ans.push_back(temp);
             return;
         }
 
-        for(int ind = i ; ind<arr.size() ; ind++){
-            if(arr[ind] > tar) break;
-            
+        sol(ind - 1 , tar , arr , temp , ans);
+
+        if(arr[ind] <= tar){
             temp.push_back(arr[ind]);
-            sol(ind , tar - arr[ind] , temp , ans , arr);
+            sol(ind , tar - arr[ind] , arr , temp , ans);
             temp.pop_back();
         }
+
+        return;
     }
     vector<vector<int>> combinationSum(vector<int>& arr, int tar) {
-        sort(arr.begin(), arr.end());
-        int n = arr.size();
         vector<vector<int>> ans;
         vector<int> temp;
-        sol(0 , tar , temp , ans , arr);
+        int n = arr.size();
+
+        sol(n - 1 , tar , arr , temp , ans);
+
         return ans;
     }
 };
