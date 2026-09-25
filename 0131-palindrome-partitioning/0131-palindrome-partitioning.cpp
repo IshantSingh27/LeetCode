4b55857b132b9386bfe05ece5d776f1416cf1e70
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-09-25 19:02:39 +0530

    Time: 62 ms (27.39%), Space: 61.8 MB (43.36%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    bool palin(int s , int e , string arr){
-        while(s <= e){
-            if(arr[s++] != arr[e--]){
-                return false;
-            }
+    bool chk(int i , int j , string s){
+        while(i <= j){
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
         }
-
         return true;
     }
-    
-    void sol(int ind, string s,  vector<string>& temp, vector<vector<string>>& ans){
-        if(ind == s.size()){
+    void sol(int i , string s , vector<string>& temp , vector<vector<string>>& ans){
+        if(i == s.size()){
             ans.push_back(temp);
             return;
         }
 
-        for(int i=ind ; i<s.size() ; i++){
-            if(palin(ind , i , s)){
-                temp.push_back(s.substr(ind , i - ind + 1));
-                sol(i + 1 , s , temp , ans);
+        for(int j=i ; j<s.size() ; j++){
+            if(chk(i , j , s)){
+                temp.push_back(s.substr(i , j - i + 1));
+                sol(j + 1 , s , temp , ans);
                 temp.pop_back();
             }
         }
     }
-    
     vector<vector<string>> partition(string s) {
-        vector<string> temp;
         vector<vector<string>> ans;
+        vector<string> temp;
 
-        sol(0,s,temp,ans);
+        sol(0 , s , temp , ans);
 
         return ans;
     }
